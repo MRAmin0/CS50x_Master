0b8c4ce505b43c8d15e612e0ee9ebef92259437d
MRAm <125295545+MRAmin0@users.noreply.github.com>
2023-07-30 14:52:05 +0000

    Sun, Jul 30, 2023, 7:22 PM +04:30

--- CS50x/PSET1/hello/hello.c
+++ CS50x/PSET1/hello/hello.c
@@ -1,8 +1,10 @@
-#include <stdio.h>
 #include <cs50.h>
+#include <stdio.h>
 
 int main(void)
 {
+    //get user name
     string name = get_string("What's your name? ");
+    //say hello to user's name
     printf("hello, %s\n", name);
 }
