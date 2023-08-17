1b78f03d3e0adc9586bc604e0e7d4f72c4477c57
MRAm <125295545+MRAmin0@users.noreply.github.com>
2023-08-17 10:06:41 +0000

    Thu, Aug 17, 2023, 2:36 PM +04:30

--- CS50x/PSET2/readability/readability.c
+++ CS50x/PSET2/readability/readability.c
@@ -1,7 +1,20 @@
 #include <cs50.h>
 #include <stdio.h>
+#include 
 
-int main(void) 
+int main(void)
 {
+    string text = get_string("Text: ");
+
+    int l = 0;
+    int n = strlen(text);
+    for (int i = 0; i <n; i++)
+    {
+        if (isalpha(text[i] == 1 ) || isalpha(text[i]) == 2)
+        {
+            l++;
+        }
+    }
+    printf("l= %i",l);
 
 }
