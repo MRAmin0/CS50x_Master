33164ff222c66d8e758a45db832fe436a425cc36
MRAm <125295545+MRAmin0@users.noreply.github.com>
2023-08-17 20:53:39 +0000

    Fri, Aug 18, 2023, 1:23 AM +04:30

--- CS50x/PSET4/filter-less/helpers.c
+++ CS50x/PSET4/filter-less/helpers.c
@@ -4,24 +4,22 @@
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
-    int redmain,greenmain , bluemain;
+    int redmain, greenmain, bluemain;
     float grayout;
 
-    for(int i = 0; i < height; i++)
+    for (int i = 0; i < height; i++)
     {
-        for(int j = 0; j < width; j++)
+        for (int j = 0; j < width; j++)
         {
-            redmain = image [i][j].rgbtRed;
-            greenmain = image [i][j].rgbtGreen;
-            bluemain = image [i][j].rgbtBlue;
+            redmain = image[i][j].rgbtRed;
+            greenmain = image[i][j].rgbtGreen;
+            bluemain = image[i][j].rgbtBlue;
 
             grayout = round((redmain + greenmain + bluemain) / 3.0);
 
-            image [i][j].rgbtRed = grayout;
-            image [i][j].rgbtGreen = grayout;
-            image [i][j].rgbtBlue = grayout;
-
-
+            image[i][j].rgbtRed = grayout;
+            image[i][j].rgbtGreen = grayout;
+            image[i][j].rgbtBlue = grayout;
         }
     }
 }
@@ -29,24 +27,37 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 // Convert image to sepia
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
-        int redmain,greenmain , bluemain;
-        int redsep,greensep , bluesep;
-        for(int i = 0; i < height; i++)
+    int originalRed, originalGreen, originalBlue;
+    int sepiaRed, sepiaGreen, sepiaBlue;
+    for (int i = 0; i < height; i++)
     {
-        for(int j = 0; j < width; j++)
+        for (int j = 0; j < width; j++)
         {
-            redmain = image [i][j].rgbtRed;
-            greenmain = image [i][j].rgbtGreen;
-            bluemain = image [i][j].rgbtBlue;
+            originalRed = image[i][j].rgbtRed;
+            originalGreen = image[i][j].rgbtGreen;
+            originalBlue = image[i][j].rgbtBlue;
+            
+            sepiaRed = .393 * originalRed + .769 * originalGreen + .189 * originalBlue;
+            if (sepiaRed > 255)
+            {
+                sepiaRed = 255;
+            }
+            sepiaGreen = .349 * originalRed + .686 * originalGreen + .168 * originalBlue;
+            if (sepiaGreen > 255)
+            {
+                sepiaGreen = 255;
+            }
+            sepiaBlue = .272 * originalRed + .534 * originalGreen + .131 * originalBlue;
+            if (sepiaBlue > 255)
+            {
+                sepiaBlue = 255;
+            }
 
-            grayout = round((redmain + greenmain + bluemain) / 3.0);
-
-            image [i][j].rgbtRed = grayout;
-            image [i][j].rgbtGreen = grayout;
-            image [i][j].rgbtBlue = grayout;
+            image[i][j].rgbtRed = sepiaRed;
+            image[i][j].rgbtGreen = sepiaGreen;
+            image[i][j].rgbtBlue = sepiaBlue;
         }
     }
-
 }
 
 // Reflect image horizontally
