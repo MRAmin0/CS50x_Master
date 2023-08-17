a1251fb6939dbdbff453deb3d6050793ca917346
MRAm <125295545+MRAmin0@users.noreply.github.com>
2023-08-17 13:35:59 +0000

    Thu, Aug 17, 2023, 6:05 PM +04:30

--- CS50x/PSET2/caesar/caesar.c
+++ CS50x/PSET2/caesar/caesar.c
@@ -11,46 +11,43 @@ int main(int argc, string argv[])
         printf("Usage: ./caesar key\n");
         return 1;
     }
-
     int n = strlen(argv[1]);
     for (int i = 0; i < n; i++)
     {
-        if (!isdigit(argv[1][i]))
+        // Exit if find alphabet
+        if (isalpha(argv[i][i]))
         {
             printf("Usage: ./caesar key\n");
             return 1;
         }
     }
 
-    string plain = get_string("plaintext: ");
-    int key = atoi(argv[1]) % 26;
+    string plain = get_string("plaintext:  ");
+    // atoi : convert string to int
+    int key = atoi(argv[1]);
 
+    char co;
+    int nn = strlen(plain);
+    // char cipher[nn];
     printf("ciphertext: ");
-
-    for (int j = 0, nn = strlen(plain); j < nn; j++)
+    for (int j = 0; j < nn; j++)
     {
-        char c = plain[j];
-        char co;
-
+        int c = plain[j];
         if (isalpha(c))
         {
-            if (islower(c))
+            co = c + key % 26;
+
+            if (!(islower(co) || isupper(co)))
             {
-                co = 'a' + (c - 'a' + key) % 26;
-            }
-            else
-            {
-                co = 'A' + (c - 'A' + key) % 26;
+                co -= 26;
             }
         }
         else
         {
             co = c;
         }
-
-        printf("ciphertext: %c", co);
+        printf("%c", co);
     }
-
     printf("\n");
     return 0;
 }
