eaeb93ee6ad0cf50fdd2b45c82e91c8579a872d4
MRAm <125295545+MRAmin0@users.noreply.github.com>
2023-08-17 20:12:50 +0000

    Fri, Aug 18, 2023, 12:42 AM +04:30

--- CS50x/PSET4/smiley/helpers.c
+++ CS50x/PSET4/smiley/helpers.c
@@ -7,7 +7,12 @@ void colorize(int height, int width, RGBTRIPLE image[height][width])
     {
         for(int column = 0; column < height; column++)
         {
-            
+            if (image[column][row].rgbtBlue == 0 && image[column][row].rgbtGreen == 0 && image[column][row].rgbtRed == 0)
+            {
+                image[column][row].rgbtBlue = 55;
+                image[column][row].rgbtGreen =35;
+                image[column][row].rgbtRed = 149;
+            }
         }
     }
 }
