20e93699127ef39b99a8a586d29bf4cada1844d2
MRAm <125295545+MRAmin0@users.noreply.github.com>
2023-08-17 14:56:47 +0000

    Thu, Aug 17, 2023, 7:26 PM +04:30

--- CS50x/PSET3/runoff/runoff.c
+++ CS50x/PSET3/runoff/runoff.c
@@ -128,6 +128,14 @@ int main(int argc, string argv[])
 bool vote(int voter, int rank, string name)
 {
     // TODO
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if(strcmp(name, candidates[i].name) == 0)
+        {
+            return true;
+            preferences[voter][rank] = i;
+        }
+    }
     return false;
 }
 
