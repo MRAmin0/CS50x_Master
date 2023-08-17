168f9d941417017d19c074090b3fb014e3f39ffd
MRAm <125295545+MRAmin0@users.noreply.github.com>
2023-08-17 12:39:29 +0000

    Thu, Aug 17, 2023, 5:09 PM +04:30

--- CS50x/PSET2/caesar/caesar.c
+++ CS50x/PSET2/caesar/caesar.c
@@ -1,7 +1,21 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(int argc, string argv[]) 
+int main(int argc, string argv[])
 {
-
+    if(argc != 2)
+    {
+        printf("Usage: ./caesar key\n");
+        return 1;
+    }
+    int n = trlen(argv[1]);
+    for (int i = 0;i < n; i++)
+    {
+        // m
+        if (isalpha(argv[i][i]))
+        {
+            printf("Usage: ./caesar key\n");
+            return 1;
+        }
+    }
 }
