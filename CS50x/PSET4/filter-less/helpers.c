8ae8bd6b3e71da3ef69fb04f6a697dcd2cf7b2ef
MRAm <125295545+MRAmin0@users.noreply.github.com>
2023-08-17 22:32:25 +0000

    Fri, Aug 18, 2023, 3:02 AM +04:30

--- CS50x/PSET4/filter-less/helpers.c
+++ CS50x/PSET4/filter-less/helpers.c
@@ -72,9 +72,9 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
         }
         for (int j = 0; j < height; j++)
         {
-            image[i][j].rgbtRed = rowplace[width - j - 1].rgbtRed;
-            image[i][j].rgbtGreen = rowplace[width - j - 1].rgbtGreen;
-            image[i][j].rgbtBlue = rowplace[width - j - 1].rgbtBlue;
+            image[i][j].rgbtRed = (rowplace[width - j - 1].rgbtRed);
+            image[i][j].rgbtGreen = (rowplace[width - j - 1].rgbtGreen);
+            image[i][j].rgbtBlue =(rowplace[width - j - 1].rgbtBlue);
         }
     }
 }
