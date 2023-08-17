dd6ca197216c9a3841d1a773c51e8f07fe602556
MRAm <125295545+MRAmin0@users.noreply.github.com>
2023-08-17 22:43:10 +0000

    Fri, Aug 18, 2023, 3:13 AM +04:30

--- CS50x/PSET4/filter-less/helpers.c
+++ CS50x/PSET4/filter-less/helpers.c
@@ -77,6 +77,7 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
             image[i][j].rgbtBlue =(rowplace[width - j - 1].rgbtBlue);
         }
     }
+    return ;
 }
 
 // Blur image
