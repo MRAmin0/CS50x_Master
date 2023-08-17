cd15d697b80c97f20313d76cc5bcd663b7e4ad0c
MRAm <125295545+MRAmin0@users.noreply.github.com>
2023-08-17 12:47:23 +0000

    Thu, Aug 17, 2023, 5:17 PM +04:30

--- CS50x/PSET2/caesar/caesar.c
+++ CS50x/PSET2/caesar/caesar.c
@@ -1,5 +1,8 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
 int main(int argc, string argv[])
 {
@@ -20,7 +23,31 @@ int main(int argc, string argv[])
     }
 
     string plain = get_string("plaintext:  ");
+    // atoi : convert string to int
     int key = atio(argv[1]);
 
+    printf("ciphertext: ");
+    char ci;
+    int nn = strlen(plain);
+    char cipher[nn];
+
+
+    for (int j =0; j < nn ; j++)
+    {
+        int c = plain[j];
+        if(isalpha(c))
+        {
+            ci = c+ key % 26;
+
+            if(!(islower(ci)|| isupper(ci)))
+            {
+                ci -=26;
+            }
+        }
+        else
+        {
+            c
+        }
+    }
 
 }
