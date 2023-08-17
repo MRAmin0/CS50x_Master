e4c892c1fd92b7ad5432cd262c32f5024c828884
MRAm <125295545+MRAmin0@users.noreply.github.com>
2023-08-17 11:57:39 +0000

    Thu, Aug 17, 2023, 4:27 PM +04:30

--- CS50x/PSET2/bulbs/bulbs.c
+++ CS50x/PSET2/bulbs/bulbs.c
@@ -9,6 +9,28 @@ void print_bulb(int bit);
 int main(void)
 {
     // TODO
+    string message = get_string("Message: ")
+    int n = strlen(message);
+    for (int i = 0;i < n; i++)
+    {
+        int binary[] ={0, 0, 0, 0, 0, 0, 0, 0};
+        int decimal = message[i];
+
+        int j = 0;
+        while (decimal > 0)
+        {
+            binary[j] = deciaml % 2;
+            decimal = decimal / 2;
+            j++;
+        }
+
+        for (int k =BITS_IN_BYTE -1 ; k <= 0; k--)
+        {
+            
+        }
+
+
+    }
 }
 
 void print_bulb(int bit)
