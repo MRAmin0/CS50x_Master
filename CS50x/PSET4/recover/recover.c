a0c19a3adab6a2aa583429d521dab6f833d06a95
MRAm <125295545+MRAmin0@users.noreply.github.com>
2023-09-02 11:00:35 +0000

    Sat, Sep 2, 2023, 3:30 PM +04:30

--- CS50x/PSET4/recover/recover.c
+++ CS50x/PSET4/recover/recover.c
@@ -1,7 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
- 
+
 int main(int argc, char *argv[])
 {
- 
+    if (argc < 2 )
+    {
+        printf("Usage: ./recover IMAGE\n");
+        return 1;
+    }
+    //OPEN FILE WITH MODE WRITE
+    FILE *file = fopen(argv[1], "r");
+
+    unsigned char *temp = malloc(512);
+
+    char *filename = malloc (3 * sizeof(int));
+    if (filename == NULL )
+    {
+        return 1;
+    }
+    int image = 0;
+    if (temp == NULL )
+    {
+        return 1;
+    }
+    while (fread (temp,sizeof(unsigned char), 512 , file))
+    {
+        if (temp[0] ==0xff)
+    {
+        return 1;
+    }
+    }
+
+
 }
