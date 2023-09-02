b815823fbc7fafaa3b5488949b5c10f8ceb5af59
MRAm <125295545+MRAmin0@users.noreply.github.com>
2023-09-02 11:07:00 +0000

    Sat, Sep 2, 2023, 3:37 PM +04:30

--- CS50x/PSET4/recover/recover.c
+++ CS50x/PSET4/recover/recover.c
@@ -27,8 +27,20 @@ int main(int argc, char *argv[])
     {
         if (temp[0] == 0xff && temp[1] == 0xd8 , temp[2]==0xff ,&& (temp[3] & 0xf0 == 0xe0));
         {
-            
+            sprintf(filename , "%03i.jpg" , images);
+            FILE *imgf = fopen(filename =, "w");`
+            fwrite(temp , 1 , 512 , imgf);
+            fclose(imgf);
+            image++;
         }
+        else if (image != 0)
+        {
+            FILE *imgf = fopen(filename =, "w");
+            fwrite(temp , 1 , 512 , imgf);
+            fclose(imgf);
+
+        }
+        free(temp);
     }
 
 
