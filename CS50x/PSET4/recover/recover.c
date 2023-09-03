d11c11a2af39cd6a341358338ce5183d948c15cf
MRAm <125295545+MRAmin0@users.noreply.github.com>
2023-09-03 10:16:25 +0000

    Sun, Sep 3, 2023, 2:46 PM +04:30

--- CS50x/PSET4/recover/recover.c
+++ CS50x/PSET4/recover/recover.c
@@ -1,3 +1,53 @@
+// #include <stdio.h>
+// #include <stdlib.h>
+
+// int main(int argc, char *argv[])
+// {
+//     if (argc != 2)
+//     {
+//         printf("Usage: ./recover IMAGE\n");
+//         return 1;
+//     }
+//     // OPEN FILE WITH MODE WRITE
+//     FILE *drive = fopen(argv[1], "r");
+
+//     unsigned char *temp = malloc(512);
+//     char *filename = malloc(8 * sizeof(char));
+//     int image = 0;
+
+//     if (temp == NULL)
+//     {
+//         return 1;
+//     }
+
+//     if (filename == NULL)
+//     {
+//         return 1;
+//     }
+
+//     while (fread(temp, sizeof(unsigned char), 512, drive))
+//     {
+//         if (temp[0] == 0xff && temp[1] == 0xd8 && temp[2] == 0xff && (temp[3] & 0xf0) == 0xe0)
+//         {
+//             sprintf(filename, "%03i.jpg", image);
+//             FILE *imgf = fopen(filename, "a");
+//             fwrite(temp, 1, 512, imgf);
+//             fclose(imgf);
+//             image++;
+//             return 0;
+//         }
+//         else if (image != 0)
+//         {
+//             FILE *imgf = fopen(filename, "a");
+//             fwrite(temp, 1, 512, imgf);
+//             fclose(imgf);
+//         }
+//         free(temp);
+//         fclose(drive);
+//         free(filename);
+//         return 0;
+//     }
+// }
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -8,33 +58,28 @@ int main(int argc, char *argv[])
         printf("Usage: ./recover IMAGE\n");
         return 1;
     }
-    // OPEN FILE WITH MODE WRITE
-    FILE *drive = fopen(argv[1], "r");
 
+    FILE *drive = fopen(argv[1], "r");
     unsigned char *temp = malloc(512);
     char *filename = malloc(8 * sizeof(char));
     int image = 0;
-
     if (temp == NULL)
     {
         return 1;
     }
-
-    if (filename == NULL)
+    else if (filename == NULL)
     {
         return 1;
     }
-
     while (fread(temp, sizeof(unsigned char), 512, drive))
     {
         if (temp[0] == 0xff && temp[1] == 0xd8 && temp[2] == 0xff && (temp[3] & 0xf0) == 0xe0)
         {
             sprintf(filename, "%03i.jpg", image);
-            FILE *imgf = fopen(filename, "a");
+            FILE *imgf = fopen(filename, "w");
             fwrite(temp, 1, 512, imgf);
             fclose(imgf);
             image++;
-            return 0;
         }
         else if (image != 0)
         {
@@ -42,9 +87,8 @@ int main(int argc, char *argv[])
             fwrite(temp, 1, 512, imgf);
             fclose(imgf);
         }
-        free(temp);
-        fclose(drive);
-        free(filename);
-        return 0;
     }
+    free(temp);
+    free(filename);
+    fclose(drive);
 }
