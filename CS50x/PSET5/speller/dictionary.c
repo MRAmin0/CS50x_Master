681958975f083b5f28ce90c6ef5ab2c728337751
MRAm <125295545+MRAmin0@users.noreply.github.com>
2023-09-03 11:39:59 +0000

    Sun, Sep 3, 2023, 4:09 PM +04:30

--- CS50x/PSET5/speller/dictionary.c
+++ CS50x/PSET5/speller/dictionary.c
@@ -2,6 +2,12 @@
 
 #include <ctype.h>
 #include <stdbool.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <strings.h>
+
 
 #include "dictionary.h"
 
@@ -14,15 +20,18 @@ typedef struct node
 node;
 
 // TODO: Choose number of buckets in hash table
-const unsigned int N = 26;
+const unsigned int N = 100000;
 
 // Hash table
 node *table[N];
+
+int dict_size = 0;
 
 // Returns true if word is in dictionary, else false
 bool check(const char *word)
 {
     // TODO
+
     return false;
 }
 
@@ -37,7 +46,30 @@ unsigned int hash(const char *word)
 bool load(const char *dictionary)
 {
     // TODO
-    return false;
+    FILE *dict_pointer = fopen(dictionary , "r")
+    if (dictionary == NULL)
+    {
+        printf("Unable to open %s\n",dictionary);
+        return false;
+    }
+    char next_word[LENGTH + 1];
+    while (fscanf(dict_pointer , "%s", next_word) != EOF)
+    {
+        note *n = malloc(sizeof(node));
+        if (n == NULL)
+        {
+            return false;
+        }
+        strcpy(n -> word , next_word);
+        int hash_value = hash(next_word);
+        n -> nexet = table[hash_value];
+        table[hash_value] = n;
+        dict_size++;
+
+    }
+    fclose(dict_pointer);
+
+    return true;
 }
 
 // Returns number of words in dictionary if loaded, else 0 if not yet loaded
