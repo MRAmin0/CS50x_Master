0c7be7e067b8d2b6074c8fd2b7c046105be53309
MRAm <125295545+MRAmin0@users.noreply.github.com>
2023-09-03 11:14:09 +0000

    Sun, Sep 3, 2023, 3:44 PM +04:30

--- CS50x/PSET5/inheritance/inheritance.c
+++ CS50x/PSET5/inheritance/inheritance.c
@@ -40,6 +40,11 @@ int main(void)
 person *create_family(int generations)
 {
     // TODO: Allocate memory for new person
+    person *child = malloc(sizeof(person));
+    if (child == NULL)
+    {
+        return NULL;
+    }
 
     // If there are still generations left to create
     if (generations > 1)
@@ -50,6 +55,7 @@ person *create_family(int generations)
 
         // TODO: Set parent pointers for current person
 
+        
         // TODO: Randomly assign current person's alleles based on the alleles of their parents
 
     }
