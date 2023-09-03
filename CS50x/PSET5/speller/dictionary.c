a75ec04fc9be0796c42ff83a9941e0a2ec1d175a
MRAm <125295545+MRAmin0@users.noreply.github.com>
2023-09-03 11:42:16 +0000

    Sun, Sep 3, 2023, 4:12 PM +04:30

--- CS50x/PSET5/speller/dictionary.c
+++ CS50x/PSET5/speller/dictionary.c
@@ -39,7 +39,13 @@ bool check(const char *word)
 unsigned int hash(const char *word)
 {
     // TODO: Improve this hash function
-    return toupper(word[0]) - 'A';
+    long sum = 0;
+    for(int i = 0; i < strlen(word); i++)
+    {
+        sum+= tolower(word[i]);
+
+    }
+    return sum % N;
 }
 
 // Loads dictionary into memory, returning true if successful, else false
