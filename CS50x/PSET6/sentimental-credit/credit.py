27b8c9817ddff5b107ab09c7d36579a4c36c8762
MRAm <125295545+MRAmin0@users.noreply.github.com>
2023-09-05 14:51:50 +0000

    Tue, Sep 5, 2023, 7:21 PM +04:30

--- CS50x/PSET6/sentimental-credit/credit.py
+++ CS50x/PSET6/sentimental-credit/credit.py
@@ -1,4 +1,5 @@
 from cs50 import get_string
+
 while True:
     cardnum = get_string("Number: ")
     if cardnum.isnumeric():
@@ -13,9 +14,9 @@ card = reversed([int(digit) for digit in cardnum])
 # charkhidan be tedad monaseb
 for i, digit in enumerate(card):
     if (i + 1) % 2 == 0:
-        digitodd = digit *2
+        digitodd = digit * 2
         if digitodd > 9:
-            odds += int(digitodd /10) + digitodd % 10
+            odds += int(digitodd / 10) + digitodd % 10
         else:
             odds += digitodd
     else:
@@ -28,11 +29,11 @@ cardlen = len(cardnum)
 sld = sum % 10
 
 if sld == 0:
-    if start in [34,37] and cardlen == 15:
-        print("AMERICAN EXPRESS")
-    elif (start in range (51,56) and cardlen == 16):
+    if start in [34, 37] and cardlen == 15:
+        print("AMEX")
+    elif start in range(51, 56) and cardlen == 16:
         print("MASTERCARD")
-    elif ((int(cardnum[0]) == 4) and cardlen in [13,16]):
+    elif (int(cardnum[0]) == 4) and cardlen in [13, 16]:
         print("VISA")
 else:
     print("INVALID")
