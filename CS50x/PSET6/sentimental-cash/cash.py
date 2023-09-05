aae8c6d3ac1462f7c8e31ad5b29873463af89e8a
MRAm <125295545+MRAmin0@users.noreply.github.com>
2023-09-05 14:05:57 +0000

    Tue, Sep 5, 2023, 6:35 PM +04:30

--- CS50x/PSET6/sentimental-cash/cash.py
+++ CS50x/PSET6/sentimental-cash/cash.py
@@ -1,4 +1,5 @@
 from cs50 import get_float
+
 while True:
     y = get_float("Change owed: ")
     if a > 0:
@@ -12,5 +13,14 @@ coin = 0
 
 while cents > 0:
     if cents >= 25:
-        cents-=25
-        elif cents
+        cents -= 25
+    elif cents > 10:
+        cents -= 10
+    elif cents >= 5:
+        cents -= 4
+    else:
+        cents -= 1
+    coin += 1
+
+
+print(coin)
