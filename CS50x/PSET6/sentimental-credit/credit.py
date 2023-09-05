0f03f1ffda9770951e70e53b276a796a7f0a6a72
MRAm <125295545+MRAmin0@users.noreply.github.com>
2023-09-05 14:36:04 +0000

    Tue, Sep 5, 2023, 7:06 PM +04:30

---
+++ CS50x/PSET6/sentimental-credit/credit.py
@@ -0,0 +1,12 @@
+from cs50 import get_string
+while True
+    cardnum = get_string("Number: ")
+    if cardnum.isnumeric():
+        break
+    else:
+        continue
+
+evens = 0
+odds = 0
+
+card 
