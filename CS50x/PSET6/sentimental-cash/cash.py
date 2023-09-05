b8f324f4520c1c641f6984587dc87bf7bb6f12c9
MRAm <125295545+MRAmin0@users.noreply.github.com>
2023-09-05 14:01:09 +0000

    Tue, Sep 5, 2023, 6:31 PM +04:30

--- CS50x/PSET6/sentimental-cash/cash.py
+++ CS50x/PSET6/sentimental-cash/cash.py
@@ -1,1 +1,16 @@
-import cs50
+from cs50 import get_float
+while True:
+    y = get_float("Change owed: ")
+    if a > 0:
+        break
+    else:
+        continue
+
+
+cents = round(a * 100)
+coin = 0
+
+while cents > 0:
+    if cents >= 25:
+        cents-=25
+        elif cents
