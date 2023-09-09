3fdb290bfc6d2394e5e9cfdfda7e9c1c6e57fec6
MRAm <125295545+MRAmin0@users.noreply.github.com>
2023-09-09 11:00:36 +0000

    Sat, Sep 9, 2023, 2:30 PM +03:30

--- CS50x/PSET6/sentimental-readability/readability.py
+++ CS50x/PSET6/sentimental-readability/readability.py
@@ -4,26 +4,48 @@ from cs50 import get_string
 text = get_string("Text: ")
 
 letters = 0
-words = 0
+words = 1
 sentences = 0
 
 # count number of letters words and sentences
 for i in text:
     if i.isalpha():
         letters += 1
-
-# remove space in word
-words = len(text.split())
-for j in text:
-    if j == "." or j == "?" or j == "!":
+    elif i == " ":
+        words += 1
+    elif i == "." or i == "!" or i == "?":
         sentences += 1
-wordsentence = words / 100
-L = letters / wordsentence
-S = sentences / wordsentence
-index = round(0.588 * L - 0.296 * S - 15.8)
-if index < 1:
-    print("Before Grade 1")
-elif index >= 16:
-    print("Grade 16+")
-else:
-    print(f"Grade {index}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # remove space in word
+# words = len(text.split())
+# for j in text:
+#     if j == "." or j == "?" or j == "!":
+#         sentences += 1
+# wordsentence = words / 100
+# L = letters / wordsentence
+# S = sentences / wordsentence
+# index = round(0.588 * L - 0.296 * S - 15.8)
+# if index < 1:
+#     print("Before Grade 1")
+# elif index >= 16:
+#     print("Grade 16+")
+# else:
+#     print(f"Grade {index}")
