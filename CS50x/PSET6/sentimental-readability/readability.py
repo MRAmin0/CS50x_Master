bddb79b16c8192c7e201232f96bb8593e9942a11
MRAm <125295545+MRAmin0@users.noreply.github.com>
2023-09-09 10:47:49 +0000

    Sat, Sep 9, 2023, 2:17 PM +03:30

--- CS50x/PSET6/sentimental-readability/readability.py
+++ CS50x/PSET6/sentimental-readability/readability.py
@@ -4,16 +4,16 @@ from cs50 import get_string
 text = get_string("Text: ")
 letters = 0
 for i in text:
-    if i.isalpha():
+    if (i.isalpha()):
         letters += 1
 
 # remove space in word
-word = len(text.split())
+words = len(text.split())
 sentences = 0
 for j in text:
     if j == '.' or j == '?' or j == '!':
         sentences += 1
-wordsentence = word / 100
+wordsentence = words / 100
 L = letters / wordsentence
 S = sentences / wordsentence
 index = round(0.588 * L - 0.296 * S - 15.8)
