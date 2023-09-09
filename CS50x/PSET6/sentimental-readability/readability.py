5732bc82cb4a885b5305b6268ff4b0bbc8655424
MRAm <125295545+MRAmin0@users.noreply.github.com>
2023-09-09 10:44:24 +0000

    Sat, Sep 9, 2023, 2:14 PM +03:30

--- CS50x/PSET6/sentimental-readability/readability.py
+++ CS50x/PSET6/sentimental-readability/readability.py
@@ -1,25 +1,59 @@
+# import string
+# from cs50 import get_string
+
+# text = get_string("Text: ")
+# letters = 0
+# for i in text:
+#     if i.isalpha():
+#         letters += 1
+
+# # remove space in word
+# word = len(text.split())
+# sentences = 0
+# for j in text:
+#     if j == '.' or j == '?' or j == '!':
+#         sentences += 1
+# wordsentence = word / 100
+# L = letters / wordsentence
+# S = sentences / wordsentence
+# index = round(0.588 * L - 0.296 * S - 15.8)
+# if index < 1:
+#     print("Before Grade 1")
+# elif index >= 16:
+#     print("Grade 16+")
+# else:
+#     print(f"Grade {index}")
+
 import string
 from cs50 import get_string
 
 text = get_string("Text: ")
 letters = 0
-for i in text:
-    if i.isalpha():
+
+for char in text:
+    if char.isalpha():
         letters += 1
 
-# remove space in word
+# Remove space in word
 word = len(text.split())
 sentences = 0
-for j in text:
-    if j == '.' or j == '?' or j == '!':
+
+for char in text:
+    if char in ['.', '?', '!']:
         sentences += 1
-wordsentence = word / 100
-L = letters / wordsentence
-S = sentences / wordsentence
-index = round(0.588 * L - 0.296 * S - 15.8)
-if index < 1:
-    print("Before Grade 1")
-elif index >= 16:
-    print("Grade 16+")
+
+# Avoid division by zero
+if word == 0:
+    print("No words found in the text.")
 else:
-    print(f"Grade {index}")
+    wordsentence = word / 100
+    L = (letters / wordsentence) * 0.588
+    S = (sentences / wordsentence) * 0.296
+    index = round(0.0588 * L - 0.296 * S - 15.8)
+
+    if index < 1:
+        print("Before Grade 1")
+    elif index >= 16:
+        print("Grade 16+")
+    else:
+        print(f"Grade {index}")
