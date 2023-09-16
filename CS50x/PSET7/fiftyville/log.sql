572fa484efa5005f8ea490037373d4dc45fae43c
MRAm <125295545+MRAmin0@users.noreply.github.com>
2023-09-16 11:51:27 +0000

    Sat, Sep 16, 2023, 3:21 PM +03:30

--- CS50x/PSET7/fiftyville/log.sql
+++ CS50x/PSET7/fiftyville/log.sql
@@ -1,4 +1,5 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
+
 -- checking the events on the given date and street
 SELECT
   description
@@ -8,9 +9,15 @@ WHERE
   YEAR = 2021
   AND MONTH = 7
   AND DAY = 28
-  AND street = 'Humphrey Street'
+  AND street = 'Humphrey Street';
 
-
---- checking the interviews with people
-select name, transcript
-from interviews
+-- checking the interviews with people
+SELECT
+  name,
+  transcript
+FROM
+  interviews
+WHERE
+  YEAR = 2021
+  AND MONTH = 7
+  AND DAY = 28;
