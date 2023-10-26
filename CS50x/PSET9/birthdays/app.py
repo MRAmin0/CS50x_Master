e1cd8362702498842e0aef146803baaed65fe967
MRAm <125295545+MRAmin0@users.noreply.github.com>
2023-10-26 20:53:59 +0000

    Fri, Oct 27, 2023, 12:23 AM +03:30

--- CS50x/PSET9/birthdays/app.py
+++ CS50x/PSET9/birthdays/app.py
@@ -37,9 +37,13 @@ def index():
         elif not inputmonth:
             message = "Please Enter Month ! "
         else:
-            db.execute("INSERT INTO birthdays(name,month,day) VALUES(?,?,?)",inputname,inputmonth,inputday)
-            birthdays = db.execute("SELECT * FROM birthdays")
-            retrun render_template("index.html", message = message , birthdays = birthdays)
+            db.execute("INSERT INTO birthdays(name,month,day) VALUES(?,?,?)",
+                        inputname,
+                        inputmonth,
+                        inputday
+                    )
+        birthdays = db.execute("SELECT * FROM birthdays")
+        return render_template("index.html", message = message , birthdays = birthdays)
 
     else:
         # TODO: Display the entries in the database on index.html
