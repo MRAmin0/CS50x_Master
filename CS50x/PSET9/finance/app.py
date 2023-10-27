19a01bdff9d932639ef29514871aa1d7425ca6c8
MRAm <125295545+MRAmin0@users.noreply.github.com>
2023-10-27 08:40:14 +0000

    Fri, Oct 27, 2023, 12:10 PM +03:30

--- CS50x/PSET9/finance/app.py
+++ CS50x/PSET9/finance/app.py
@@ -3,10 +3,10 @@ import os
 
 from cs50 import SQL
 from flask import Flask, flash, redirect, render_template, request, session
-from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
 from helpers import apology, login_required, lookup, usd
 from flask_session import Session
+
 
 # Configure application
 app = Flask(__name__)
