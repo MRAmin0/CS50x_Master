7ccf046a409827ddafa9e3bfd924c254328be188
MRAm <125295545+MRAmin0@users.noreply.github.com>
2023-10-27 09:15:03 +0000

    Fri, Oct 27, 2023, 12:45 PM +03:30

--- CS50x/PSET9/finance/app.py
+++ CS50x/PSET9/finance/app.py
@@ -183,7 +183,7 @@ def login():
         if len(rows) != 1 or not check_password_hash(
             rows[0]["hash"], request.form.get("password")
         ):
-            return apology("invalid username and/or password", 403)
+            return apology("invalid username or password", 403)
 
         # Remember which user has logged in
         session["user_id"] = rows[0]["id"]
