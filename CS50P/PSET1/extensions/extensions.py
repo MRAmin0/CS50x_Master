2e62e03fe7fa09a6dcb06f4ddeb78e3d47af14e7
MRAm <125295545+MRAmin0@users.noreply.github.com>
2023-11-01 18:08:05 +0000

    Wed, Nov 1, 2023, 9:38 PM +03:30

--- CS50P/PSET1/extensions/extensions.py
+++ CS50P/PSET1/extensions/extensions.py
@@ -14,4 +14,6 @@ a = input("File name: ").strip().lower()
 if a.rfind(".") != -1:
     index = int(a.rindex(".")) + 1
     b = a[index:]
-    print(b)
+   # print(b)
+if b not in l:
+    print("application/octe")
