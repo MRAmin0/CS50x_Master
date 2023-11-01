5e0e18f0fefe27760943ba044fa23f5771be99e2
MRAm <125295545+MRAmin0@users.noreply.github.com>
2023-11-01 17:47:58 +0000

    Wed, Nov 1, 2023, 9:17 PM +03:30

--- CS50P/PSET1/deep/deep.py
+++ CS50P/PSET1/deep/deep.py
@@ -1,3 +1,12 @@
-a = ["42" , "forty-two" ,"forty two"]
-q = input ("What is the Answer to the Great Question of Life, the Universe, and Everything?")
-
+a = ["42", "forty-two", "forty two"]
+q = (
+    input(
+        "What is the Answer to the Great Question of Life, the Universe, and Everything? "
+    )
+    .strip()
+    .lower()
+)
+if q in a:
+    print("Yes")
+else:
+    print("No")
