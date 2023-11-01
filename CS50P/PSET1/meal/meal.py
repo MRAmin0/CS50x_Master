d7c7e8619a05f051bb2f51bb3653f7b2fc5f6889
MRAm <125295545+MRAmin0@users.noreply.github.com>
2023-11-01 19:47:22 +0000

    Wed, Nov 1, 2023, 11:17 PM +03:30

--- CS50P/PSET1/meal/meal.py
+++ CS50P/PSET1/meal/meal.py
@@ -1,29 +1,30 @@
 def main():
-    sch =
-    [
-         {"meal" : "breakfast time" , "start hour" : 7, "end hour" :8},
-         {"meal" : "lunch time" , "start hour" : 12, "end hour" :13},
-         {"meal" : "dinner time" , "start hour" : 18, "end hour" :19},
+    sch = [
+        {"meal": "breakfast time", "start hour": 7, "end hour": 8},
+        {"meal": "lunch time", "start hour": 12, "end hour": 13},
+        {"meal": "dinner time", "start hour": 18, "end hour": 19},
     ]
     time = input("what time is it? ")
     time = float(convert(time))
-    for d in sch
+    for d in sch:
         if time >= float(d["start hour"]) and time <= float(d["end hour"]):
             print(d["meal"])
-        else
+        else:
             continue
+
 
 def convert(time):
     c = 0.0
-    if time rfind(" a.m.") != -1:
-        time = time.reaplace(" a.m." ,"")
-    elif time rfind(" p.m.") != -1:
-        time = time.reaplace(" p.m." ,"")
+    if time.rfind(" a.m.") != -1:
+        time = time.reaplace(" a.m.", "")
+    elif time.rfind(" p.m.") != -1:
+        time = time.reaplace(" p.m.", "")
         c = 12.0
-
 
     h, m = time.strip().split(":")
     t = float(h) + c + (float(m) / 60)
     return t
+
+
 if __name__ == "__main__":
     main()
