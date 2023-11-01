eddb69b404082f025151cc83ded7576c0d192b42
MRAm <125295545+MRAmin0@users.noreply.github.com>
2023-11-01 19:35:19 +0000

    Wed, Nov 1, 2023, 11:05 PM +03:30

--- CS50P/PSET1/meal/meal.py
+++ CS50P/PSET1/meal/meal.py
@@ -1,7 +1,9 @@
 def main():
     sch =
     [
-         {"meal" : "breakfast time" , "start hour" : }
+         {"meal" : "breakfast time" , "start hour" : 7, "end hour" :8}
+         {"meal" : "breakfast time" , "start hour" : 7, "end hour" :8}
+         {"meal" : "breakfast time" , "start hour" : 7, "end hour" :8}
     ]
 
 
