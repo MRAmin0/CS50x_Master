ef9aa8a9639118a5064aba7acb0ec8e5d346b438
MRAm <125295545+MRAmin0@users.noreply.github.com>
2023-11-01 19:36:39 +0000

    Wed, Nov 1, 2023, 11:06 PM +03:30

--- CS50P/PSET1/meal/meal.py
+++ CS50P/PSET1/meal/meal.py
@@ -2,10 +2,11 @@ def main():
     sch =
     [
          {"meal" : "breakfast time" , "start hour" : 7, "end hour" :8}
-         {"meal" : "breakfast time" , "start hour" : 7, "end hour" :8}
-         {"meal" : "breakfast time" , "start hour" : 7, "end hour" :8}
+         {"meal" : "lunch time" , "start hour" : 12, "end hour" :13}
+         {"meal" : "dinner time" , "start hour" : 18, "end hour" :19}
     ]
-
+    time = input("what time is it? ")
+    time = float(convert(time))
 
 def convert(time):
     ...
