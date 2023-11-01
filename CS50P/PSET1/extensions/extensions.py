7511ed53ff0020c701c83467e06f43fa02c7fe1c
MRAm <125295545+MRAmin0@users.noreply.github.com>
2023-11-01 18:05:12 +0000

    Wed, Nov 1, 2023, 9:35 PM +03:30

--- CS50P/PSET1/extensions/extensions.py
+++ CS50P/PSET1/extensions/extensions.py
@@ -10,6 +10,7 @@ l = {
 
 a = input("File name: ").strip().lower()
 
+# rfind() method finds the last occurrence of the specified value
 if a.rfind(".") != -1:
     index = int(a.rindex(".")) + 1
     b = a[index:]
