99a4926440caa66bcd9e0682c60c2951dcbb2f13
MRAm <125295545+MRAmin0@users.noreply.github.com>
2023-11-01 19:28:21 +0000

    Wed, Nov 1, 2023, 10:58 PM +03:30

--- CS50P/PSET1/interpreter/interpreter.py
+++ CS50P/PSET1/interpreter/interpreter.py
@@ -1,3 +1,10 @@
-x, y, z = imput("Enter an exoression : ").strip().split(" ")
+x, y, z = input("Enter an exoression : ").strip().split(" ")
 if y == "+":
     answer = float(x) + float(z)
+elif y == "-":
+    answer = float(x) - float(z)
+elif y == "*":
+    answer = float(x) * float(z)
+elif y == "/":
+    answer = float(x) / float(z)
+print(f"{answer:.1f}")
