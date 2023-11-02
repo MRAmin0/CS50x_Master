1b1723303825f38e0e82a5e1769ce29352edbe94
MRAm <125295545+MRAmin0@users.noreply.github.com>
2023-11-02 20:04:18 +0000

    Thu, Nov 2, 2023, 11:34 PM +03:30

--- CS50P/PSET3/outdated/outdated.py
+++ CS50P/PSET3/outdated/outdated.py
@@ -13,23 +13,23 @@ months = [
     "November",
     "December"
 ]
-While True:
+while True:
     try:
-    # get input from user
-    a = input("Date: ").strip()
+        # get input from user
+        a = input("Date: ").strip()
 
-    # if the input contains spaces
-    if " " in a:
-        if "," in a:
-            month , day ,year = a.split(" ")
-            # find the month title in list
-            if month.title() in months:
-                # delete the ',' after day number
-                day = int(day.strip(","))
-                # month index + 1 because list starts from 0
-                month = int(months.index(month)) + 1
-                if month <= 12 and day <= 31:
-                    # print them "2 raghami"
+        # if the input contains spaces
+        if " " in a:
+            if "," in a:
+                month , day ,year = a.split(" ")
+                # find the month title in list
+                if month.title() in months:
+                    # delete the ',' after day number
+                    day = int(day.strip(","))
+                    # month index + 1 because list starts from 0
+                    month = int(months.index(month)) + 1
+                    if month <= 12 and day <= 31:
+                        # print them "2 raghami"
                     print(f"{year}-{month:02}-{day:02}")
                     break
         # if the input had no space
