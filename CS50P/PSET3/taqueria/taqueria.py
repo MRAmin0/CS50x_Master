68b55fde43205c13fe9d8e262a7767b9bfd7208b
MRAm <125295545+MRAmin0@users.noreply.github.com>
2023-11-02 19:35:46 +0000

    Thu, Nov 2, 2023, 11:05 PM +03:30

--- CS50P/PSET3/taqueria/taqueria.py
+++ CS50P/PSET3/taqueria/taqueria.py
@@ -1,5 +1,4 @@
-price =
-{
+menu = {
     "Baja Taco": 4.25,
     "Burrito": 7.50,
     "Bowl": 8.50,
@@ -8,5 +7,18 @@ price =
     "Super Burrito": 8.50,
     "Super Quesadilla": 9.50,
     "Taco": 3.00,
-    "Tortilla Salad": 8.00
+    "Tortilla Salad": 8.00,
 }
+
+b = 0
+while True:
+    # make user imput like menu and print total
+    try:
+        a = input("Item: ").strip().title()
+        if a in menu:
+            b += menu[a]
+            print(f"${b:.2f}")
+            # if CTRL + D is pressed then exit the program
+    except (EOFError, KeyError):
+        print("", end="\n")
+        quit()
