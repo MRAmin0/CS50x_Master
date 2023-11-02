19d582905b1c4131c75a7636106f68606bf7d73b
MRAm <125295545+MRAmin0@users.noreply.github.com>
2023-11-02 19:57:36 +0000

    Thu, Nov 2, 2023, 11:27 PM +03:30

--- CS50P/PSET3/outdated/outdated.py
+++ CS50P/PSET3/outdated/outdated.py
@@ -29,11 +29,14 @@ While True:
                 # month index + 1 because list starts from 0
                 month = int(months.index(month)) + 1
                 if month <= 12 and day <= 31:
+                    # print them "2 raghami"
                     print(f"{year}-{month:02}-{day:02}")
                     break
     # if the input had no space
     else:
     month , day ,year = a.split("/")
-     
+       if month <= 12 and day <= 31:
+         print(f"{int(year)}-{int(month):02}-{int(day):02}")
+         break
 
 
