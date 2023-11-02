42c83494a8a619796f1bdb95c229daf63494cd52
MRAm <125295545+MRAmin0@users.noreply.github.com>
2023-11-02 18:11:28 +0000

    Thu, Nov 2, 2023, 9:41 PM +03:30

--- CS50P/PSET2/plates/plates.py
+++ CS50P/PSET2/plates/plates.py
@@ -1,4 +1,29 @@
-symbols = [" ", "." , "?" , "!" , "," , ":" , ";" , "(" , ")" , "[" , "]" , "'", "_" , "-" , '"' , "/" , "\\", "//" , "`" , "@" , "#" , "*",]
+symbols = [
+    " ",
+    ".",
+    "?",
+    "!",
+    ",",
+    ":",
+    ";",
+    "(",
+    ")",
+    "[",
+    "]",
+    "'",
+    "_",
+    "-",
+    '"',
+    "/",
+    "\\",
+    "//",
+    "`",
+    "@",
+    "#",
+    "*",
+]
+
+
 def main():
     plate = input("Plate: ").strip()
     if is_valid(plate):
@@ -10,21 +35,22 @@ def main():
 def is_valid(s):
     validated = ""
     numcheck = 0
-    if len(s) >= 2 and len(s) <=6:
+    if len(s) >= 2 and len(s) <= 6:
         if s[0].isalpha() and s[1].isalpha():
             for ch in s:
-                if ch not in symbols :
+                if ch not in symbols:
                     if ch.isnumeric() and numcheck == 0 and ch != "0":
                         numcheck += 1
                         validated += ch
-                    elif ch.isnumeric() and numcheck > 0 :
+                    elif ch.isnumeric() and numcheck > 0:
                         numcheck += 1
                         validated += ch
-                    elif ch.isalpha() and numcheck == 0 :
+                    elif ch.isalpha() and numcheck == 0:
                         validated += ch
     if validated == s:
         return True
     else:
         return False
+
 
 main()
