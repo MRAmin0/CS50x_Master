8eb69ac629c95c3270d62783267f7270bde35c79
MRAm <125295545+MRAmin0@users.noreply.github.com>
2023-11-02 19:26:45 +0000

    Thu, Nov 2, 2023, 10:56 PM +03:30

--- CS50P/PSET3/fuel/fuel.py
+++ CS50P/PSET3/fuel/fuel.py
@@ -6,7 +6,18 @@ while True:
         u,d = tank.split("/", 1)
         # cheking if both are numbers
         if u.isdigit() and d.isdigit():
-            if int(u) <= ind(d) and int(d) != 0:
+            if int(u) < = ind(d) and int(d) != 0:
                 fuel = (int(u) / int(d)) * 100
                 if fuel >= 99:
-                    
+                    print("F")
+                    break
+                elif fuel < 99 and fuel > 1:
+                    print(f"{fuel:.0f}%")
+                    break
+                else:
+                    print("E")
+                    break
+    except (ValueError , ZeroDivisionError):
+        pass
+    else:
+        pass
