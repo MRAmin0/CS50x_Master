1fc3d2cc2b22ff958031b86a9766d1abed12b1c6
MRAm <125295545+MRAmin0@users.noreply.github.com>
2023-11-02 19:53:47 +0000

    Thu, Nov 2, 2023, 11:23 PM +03:30

--- CS50P/PSET3/outdated/outdated.py
+++ CS50P/PSET3/outdated/outdated.py
@@ -1,3 +1,4 @@
+
 months = [
     "January",
     "February",
@@ -12,4 +13,22 @@ months = [
     "November",
     "December"
 ]
+While True:
+    try:
+    # get input from user
+    a = input("Date: ").strip()
 
+    # if the input contains spaces
+    if " " in a:
+        if "," in a:
+            month , day ,year = a.split(" ")
+            # find the month title in list
+            if month.title() in months:
+                # delete the ',' after day number
+                day = int(day.strip(","))
+                # month index + 1 because list starts from 0
+                month = int(months.index(month)) + 1
+                if month <= 12 and day <= 31:
+                    print(f"{year}-{month:02}-{day:02}")
+                    break
+                
