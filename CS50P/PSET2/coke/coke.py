d16d76e5e74686dc6d3b97be9571edb2487825d7
MRAm <125295545+MRAmin0@users.noreply.github.com>
2023-11-02 17:39:24 +0000

    Thu, Nov 2, 2023, 9:09 PM +03:30

--- CS50P/PSET2/coke/coke.py
+++ CS50P/PSET2/coke/coke.py
@@ -2,7 +2,7 @@ cost = 50
 gain = 0
 while gain < cost:
     print(f"Amount Due: {cost-gain}")
-    a = int(input("Next coin: ").strip())
+    a = int(input("Input coin: ").strip())
     if a == 25 or a == 10 or a == 5:
         gain = +a
     else:
