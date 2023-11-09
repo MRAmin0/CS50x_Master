fcfbce5620881edf690d9db6f6872b0cffdee821
MRAm <125295545+MRAmin0@users.noreply.github.com>
2023-11-09 16:03:18 +0000

    Thu, Nov 9, 2023, 7:33 PM +03:30

--- CS50x/PSET9/birthdays/test.py
+++ CS50x/PSET9/birthdays/test.py
@@ -45,7 +45,7 @@ def index():
                 inputday,
             )
         birthdays = db.execute("SELECT * FROM birthdays")
-        return render_template("index.html", message=message, birthdays=birthdays)
+        return render_template("index.html", birthdays=birthdays)
 
     else:
         # TODO: Display the entries in the database on index.html
