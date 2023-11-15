44d50ba11945e45efbd4f4584ba4aec71237d1f1
MRAm <125295545+MRAmin0@users.noreply.github.com>
2023-11-15 08:18:18 +0000

    Wed, Nov 15, 2023, 11:48 AM +03:30

--- CS50P/PSET4/figlet/figlet.py
+++ CS50P/PSET4/figlet/figlet.py
@@ -7,8 +7,17 @@ fonlist = figlet.getFonts()
 # if there are no parameters in argv to be used as font choice
 if len(sys.argv) == 1:
     a = input("Input: ").strip()
-    figlet.setFont(font = choice(fontlist))
+    figlet.setFont(font=choice(fontlist))
     print(figlet.renderText(a))
 # if the arvg was complete and font could be recognized
 elif len(sys.argv) == 3:
-    if str(sys.argv[1]) in ["-f" , "--font"] and str(sys.argv[2]) in fontlist
+    if str(sys.argv[1]) in ["-f", "--font"] and str(sys.argv[2]) in fontlist:
+        a = input("Input: ").strip()
+        figlet.setFont(font=str(sys.argv[2]))
+        print(figlet.renderText(a))
+    else:
+        # happens when font is not found
+        sys.exit("Invalid usage")
+# happens when there is a problem with the whole input
+else:
+    sys.exit("Invalid usage")
