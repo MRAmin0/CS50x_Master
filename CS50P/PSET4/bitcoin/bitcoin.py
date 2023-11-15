1597da08749c4836aca30c9166fd544761e97cfe
MRAm <125295545+MRAmin0@users.noreply.github.com>
2023-11-15 20:25:46 +0000

    Wed, Nov 15, 2023, 11:55 PM +03:30

--- CS50P/PSET4/bitcoin/bitcoin.py
+++ CS50P/PSET4/bitcoin/bitcoin.py
@@ -1,1 +1,15 @@
+import sys
+import request
+
+apilink ="https://api.coindesk.com/v1/bpi/currentprice.json"
+
+def main():
+    # user ask for bitcoin pride using argv
+    print(f"${coindesk(testinput()):,.4f}")
+
+
+def testinput():
+    # check if user input is currect
+    try:
+        if len(sys.argv) < 2:
 
