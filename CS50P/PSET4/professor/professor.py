825e13f27e063d2c96ff77ec901af8d35dd0d6d6
MRAm <125295545+MRAmin0@users.noreply.github.com>
2023-11-15 14:34:35 +0000

    Wed, Nov 15, 2023, 6:04 PM +03:30

--- CS50P/PSET4/professor/professor.py
+++ CS50P/PSET4/professor/professor.py
@@ -1,16 +1,23 @@
-import random
+form random import randint
 
+# meaning of each level using 3 dicts in a list
+levellist = [{1:[0,9]}, {2:[10,99]}, {3:[100,999]}]
 
 def main():
-    ...
+    slevel = get_level()
+    pset= generate_integer(slevel)
 
+# success counter
+done = 0
+
+#ask user the random generated question
 
 def get_level():
-    ...
+
 
 
 def generate_integer(level):
-    ...
+
 
 
 if __name__ == "__main__":
