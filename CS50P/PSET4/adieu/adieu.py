dcfcf67251e7ab9493e1b1a4ecb3b4e2ac80bcb8
MRAm <125295545+MRAmin0@users.noreply.github.com>
2023-11-15 13:38:24 +0000

    Wed, Nov 15, 2023, 5:08 PM +03:30

---
+++ CS50P/PSET4/adieu/adieu.py
@@ -0,0 +1,14 @@
+import inflect
+
+p = inflect.engine()
+names = []
+
+while True:
+    # getting user input
+    try:
+        a = str(input("Name :").strip())
+        names.append(a)
+        # wait for CTRL D to stop the code
+    except (EOFError, KeyError):
+        print(f"Adieu, adieu, to{p.join(names)}", end="\n")
+        quit()
