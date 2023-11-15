c25caf9d8c1af2ef83adcbd08f9d4f42db0112ea
MRAm <125295545+MRAmin0@users.noreply.github.com>
2023-11-15 14:59:21 +0000

    Wed, Nov 15, 2023, 6:29 PM +03:30

--- CS50P/PSET4/professor/professor.py
+++ CS50P/PSET4/professor/professor.py
@@ -1,16 +1,18 @@
 from random import randint
 
 # meaning of each level using 3 dicts in a list
-levellist = [{1:[0,9]}, {2:[10,99]}, {3:[100,999]}]
+levellist = [{1: [0, 9]}, {2: [10, 99]}, {3: [100, 999]}]
+
 
 def main():
     slevel = get_level()
-    pset= generate_integer(slevel)
+    pset = generate_integer(slevel)
+
 
 # success counter
 done = 0
 
-#ask user the random generated question
+# ask user the random generated question
 for question in pset:
     # fail counter
     life = 0
@@ -20,10 +22,8 @@ for question in pset:
             ans = int(input(f"{question}= "))
             a, b = question.strip(" ").split("+")
 
-
-
-        # input vs true ans
-            if life ==2:
+            # input vs true ans
+            if life == 2:
                 print("EEE")
                 print(f"{question} = {(int(a) + int(b))}")
                 break
@@ -34,7 +34,7 @@ for question in pset:
                 done += 1
                 life = 0
                 break
-        #wrong input type
+        # wrong input type
         except ValueError:
             print("EEE")
             life += 1
@@ -50,7 +50,7 @@ def get_level():
             # if it was less than 1 or more than 3 ask again
             if level <= 3 and level > 0:
                 return level
-        #wrong input? ask again
+        # wrong input? ask again
         except ValueError:
             continue
 
@@ -63,12 +63,11 @@ def generate_integer(level):
     lower = levellist[level - 1][level][0]
     higher = levellist[level - 1][level][1]
 
-    #build problem sets according to user input
-    for _ in range(0,10):
+    # build problem sets according to user input
+    for _ in range(0, 10):
         pset.append(f"{randint(lower,higher)} + {randint(lower,higher)}")
 
     return pset
-
 
 
 if __name__ == "__main__":
