4024397ce05b418229e8a2ae3e733ef7a52d4483
MRAm <125295545+MRAmin0@users.noreply.github.com>
2023-11-15 14:19:16 +0000

    Wed, Nov 15, 2023, 5:49 PM +03:30

--- CS50P/PSET4/game/game.py
+++ CS50P/PSET4/game/game.py
@@ -1,12 +1,28 @@
 from random import randint
+
 while True:
     # ask for game level, random number will be an int smaller than level
     try:
         level = int(input("Level: ").strip())
         # checck if user input is a positive int
         if level <= 1:
-            random = randint(1,level)
+            random = randint(1, level)
             # ask user to make a guess
             while True:
-                a = int(input("Guess: ").strip())
-                # comparison of user 
+                # comparison of user input and random generated number
+                try:
+                    a = int(input("Guess: ").strip())
+                    if a > random:
+                        print("Too large!")
+                    elif a < random:
+                        print("Too small!")
+                    else:
+                        print("Just right!")
+                        break
+                    # ask again for an input (guess) if it wasn't valid
+                except (TypeError, ValueError):
+                    continue
+            break
+    # ask again for an input (level) if it wasn't valid
+    except (TypeError, ValueError):
+        continue
