3dc67faf065a0c9380178dcf090315ba72ef4da5
MRAm <125295545+MRAmin0@users.noreply.github.com>
2023-11-15 08:14:54 +0000

    Wed, Nov 15, 2023, 11:44 AM +03:30

--- CS50P/PSET4/figlet/figlet.py
+++ CS50P/PSET4/figlet/figlet.py
@@ -1,3 +1,14 @@
+import sys
+from random import choice
 from pyfiglet import Figlet
 
 figlet = Figlet()
+fonlist = figlet.getFonts()
+# if there are no parameters in argv to be used as font choice
+if len(sys.argv) == 1:
+    a = input("Input: ").strip()
+    figlet.setFont(font = choice(fontlist))
+    print(figlet.renderText(a))
+# if the arvg was complete and font could be recognized
+elif len(sys.argv) == 3:
+    if str(sys.argv[1]) in ["-f" , "--font"] and str(sys.argv[2]) in fontlist
