7ce218e0b38b502f0cec07715d6a0739f42aa989
MRAm <125295545+MRAmin0@users.noreply.github.com>
2023-11-17 12:32:51 +0000

    Fri, Nov 17, 2023, 4:02 PM +03:30

--- finance/helpers.py
+++ finance/helpers.py
@@ -17,6 +17,7 @@ def apology(message, code=400):
 
         https://github.com/jacebrowning/memegen#special-characters
         """
+
         for old, new in [
             ("-", "--"),
             (" ", "-"),
@@ -42,14 +43,14 @@ def login_required(f):
     """
     Decorate routes to require login.
 
-    http://flask.pocoo.org/docs/0.12/patterns/viewdecorators/
+    https://flask.palletsprojects.com/en/1.1.x/patterns/viewdecorators/
     """
 
     @wraps(f)
     def decorated_function(*args, **kwargs):
         if session.get("user_id") is None:
             return redirect("/login")
-        
+
         return f(*args, **kwargs)
 
     return decorated_function
@@ -58,37 +59,41 @@ def login_required(f):
 def lookup(symbol):
     """Look up quote for symbol."""
 
-    # Prepare API request
-    symbol = symbol.upper()
-    end = datetime.datetime.now(pytz.timezone("US/Eastern"))
-    start = end - datetime.timedelta(days=7)
-
-    # Yahoo Finance API
-    url = (
-        f"https://query1.finance.yahoo.com/v7/finance/download/{urllib.parse.quote_plus(symbol)}"
-        f"?period1={int(start.timestamp())}"
-        f"&period2={int(end.timestamp())}"
-        f"&interval=1d&events=history&includeAdjustedClose=true"
-    )
-
-    # Query API
+    # Contact API
     try:
-        response = requests.get(
-            url,
-            cookies={"session": str(uuid.uuid4())},
-            headers={"User-Agent": "python-requests", "Accept": "*/*"},
-        )
+        api_key = os.environ.get("API_KEY")
+        url = f"https://cloud.iexapis.com/stable/stock/{urllib.parse.quote_plus(symbol)}/quote?token={api_key}"
+        response = requests.get(url)
         response.raise_for_status()
+    except requests.RequestException:
+        return None
 
-        # CSV header: Date,Open,High,Low,Close,Adj Close,Volume
-        quotes = list(csv.DictReader(response.content.decode("utf-8").splitlines()))
-        quotes.reverse()
-        price = round(float(quotes[0]["Adj Close"]), 2)
-        return {"name": symbol, "price": price, "symbol": symbol}
-    except (requests.RequestException, ValueError, KeyError, IndexError):
+    # Parse response
+    try:
+        quote = response.json()
+        return {
+            "name": quote["companyName"],
+            "price": float(quote["latestPrice"]),
+            "symbol": quote["symbol"],
+        }
+    except (KeyError, TypeError, ValueError):
         return None
 
 
 def usd(value):
     """Format value as USD."""
+
     return f"${value:,.2f}"
+
+
+def get_time():
+    """Returns formatted local time."""
+
+    return datetime.now().strftime("%d-%m-%Y %H:%M:%S")
+
+
+def check_password(first_password, second_password):
+    """Checks the passwords to be a match. Returns a message error if they do not match."""
+
+    if not check_password_hash(first_password, second_password):
+        return apology("Passwords do not match!", code=401)
