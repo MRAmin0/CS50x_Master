a6aed20b4740f4cb2f1395f05f0b514e35c21d43
MRAm <125295545+MRAmin0@users.noreply.github.com>
2023-11-18 15:03:40 +0000

    Sat, Nov 18, 2023, 6:33 PM +03:30

--- CS50P/PSET5/test_plates/plates.py
+++ CS50P/PSET5/test_plates/plates.py
@@ -11,16 +11,17 @@ def main():
 
 
 def is_valid(s):
-    pattern = r"^[A-Za-z]{2}[A-Za-z1-9]?[A-Za-z0-9]?$"
+    pattern = r"^[A-Za-z]{2}[A-Za-z1-9]{0,1}[A-Za-z0-9]{0,1}$"
+    pattern_2 = (
+        r"^[A-Za-z]{2}[A-Za-z]{0,1}[A-Za-z]{0,1}[A-Za-z1-9]{0,1}[A-Za-z1-9]{0,1}$"
+    )
 
-    if re.match(pattern, s):
-        return True
+    if re.match(pattern, s) or re.match(pattern_2, s):
+        return bool
 
     for char in string.punctuation:
         if char in s:
             return False
-
-    return False
 
 
 if __name__ == "__main__":
