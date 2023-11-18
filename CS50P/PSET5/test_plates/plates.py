c4f4db46158a4e215a0d8722376e9782fb9cd148
MRAm <125295545+MRAmin0@users.noreply.github.com>
2023-11-18 14:50:16 +0000

    Sat, Nov 18, 2023, 6:20 PM +03:30

---
+++ CS50P/PSET5/test_plates/plates.py
@@ -0,0 +1,28 @@
+python import string
+import re
+
+
+def main():
+    plate = input("Plate: ")
+    if is_valid(plate):
+        print("Valid")
+    else:
+        print("Invalid")
+
+
+def is_valid(s):
+    pattern = r"^[A-Za-z]{2}[A-Za-z1-9]{0,1}[A-Za-z0-9]{0,1}$"
+    pattern_2 = (
+        r"^[A-Za-z]{2}[A-Za-z]{0,1}[A-Za-z]{0,1}[A-Za-z1-9]{0,1}[A-Za-z1-9]{0,1}$"
+    )
+
+    if re.match(pattern, s) or re.match(pattern_2, s):
+        return bool
+
+    for char in string.punctuation:
+        if char in s:
+            return False
+
+
+if __name__ == "__main__":
+    main()
