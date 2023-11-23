b4856ad11a824011dbf5d388775162fa94e699f1
MRAm <125295545+MRAmin0@users.noreply.github.com>
2023-11-23 11:42:42 +0000

    Thu, Nov 23, 2023, 3:12 PM +03:30

--- CS50P/PSET6/lines/lines.py
+++ CS50P/PSET6/lines/lines.py
@@ -1,4 +1,5 @@
 import sys
+# we import os to get file Address
 import os
 
 
@@ -7,4 +8,20 @@ def main():
     if len(sys.argv) < 2:
         print("Too few command-line arguments")
         sys.exit(1)
-    elif len sys.argv() > 
+    elif len(sys.argv) >2:
+        print("Too many command-line arguments")
+        sys.exit(1)
+    elif not os.path.isfile(sys.argv[1]):
+        print("File foes not exist")
+        sys.exit(1)
+
+    elif not sys.argv[1].endwith(".py"):
+        print("Not a Python file")
+        sys.exit(1)
+    else:
+        print(counter(sys.argv[1]))
+
+
+def counter(python_file):
+    # count deleting empty l
+
