c3f1890478474d1b58a47ad3bb723634d30605e9
MRAm <125295545+MRAmin0@users.noreply.github.com>
2023-11-23 10:10:13 +0000

    Thu, Nov 23, 2023, 1:40 PM +03:30

---
+++ CS50P/PSET5/test_fuel/fuel.py
@@ -0,0 +1,37 @@
+def main():
+    ...
+
+
+def convert(fraction):
+    ...
+
+
+def gauge(percentage):
+    ...
+
+
+if __name__ == "__main__":
+    main()
+while True:
+    tank = input("Fraction: ").strip()
+    # using try except to stop errors in devide
+    try:
+        # spiliting the input
+        u, d = tank.split("/", 1)
+        # cheking if both are numbers
+        if u.isdigit() and d.isdigit():
+            if int(u) <= int(d) and int(d) != 0:
+                fuel = (int(u) / int(d)) * 100
+                if fuel >= 99:
+                    print("F")
+                    break
+                elif fuel < 99 and fuel > 1:
+                    print(f"{fuel:.0f}%")
+                    break
+                else:
+                    print("E")
+                    break
+    except (ValueError, ZeroDivisionError):
+        pass
+    else:
+        pass
