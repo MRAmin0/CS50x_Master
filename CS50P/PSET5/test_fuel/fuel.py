3d786175c8ae149d46476907ecb173e6c5f72ccb
MRAm <125295545+MRAmin0@users.noreply.github.com>
2023-11-23 10:13:41 +0000

    Thu, Nov 23, 2023, 1:43 PM +03:30

--- CS50P/PSET5/test_fuel/fuel.py
+++ CS50P/PSET5/test_fuel/fuel.py
@@ -1,5 +1,29 @@
 def main():
-    ...
+    while True:
+        # left fuel input
+        try:
+        tank = str(input("Fraction: ").strip())
+        # using try except to stop errors in devide
+        try:
+            # spiliting the input
+            u, d = tank.split("/", 1)
+            # cheking if both are numbers
+            if u.isdigit() and d.isdigit():
+                if int(u) <= int(d) and int(d) != 0:
+                    fuel = (int(u) / int(d)) * 100
+                    if fuel >= 99:
+                        print("F")
+                        break
+                    elif fuel < 99 and fuel > 1:
+                        print(f"{fuel:.0f}%")
+                        break
+                    else:
+                        print("E")
+                        break
+        except (ValueError, ZeroDivisionError):
+            pass
+        else:
+            pass
 
 
 def convert(fraction):
@@ -12,26 +36,4 @@ def gauge(percentage):
 
 if __name__ == "__main__":
     main()
-while True:
-    tank = input("Fraction: ").strip()
-    # using try except to stop errors in devide
-    try:
-        # spiliting the input
-        u, d = tank.split("/", 1)
-        # cheking if both are numbers
-        if u.isdigit() and d.isdigit():
-            if int(u) <= int(d) and int(d) != 0:
-                fuel = (int(u) / int(d)) * 100
-                if fuel >= 99:
-                    print("F")
-                    break
-                elif fuel < 99 and fuel > 1:
-                    print(f"{fuel:.0f}%")
-                    break
-                else:
-                    print("E")
-                    break
-    except (ValueError, ZeroDivisionError):
-        pass
-    else:
-        pass
+
