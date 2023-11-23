4bc99bae94d314bcf3d456a393d890c2201463d5
MRAm <125295545+MRAmin0@users.noreply.github.com>
2023-11-23 09:47:46 +0000

    Thu, Nov 23, 2023, 1:17 PM +03:30

--- CS50P/PSET5/test_bank/test_bank.py
+++ CS50P/PSET5/test_bank/test_bank.py
@@ -11,4 +11,9 @@ def test_value_casesenstivity():
 def test_value_firstletter():
     # if input starts with "h" but not hello return 20
     assert value("how u doing?") == 20
-    assert value("hoo ho") == 0
+    assert value("hoo hoo hoo") == 20
+
+def test_value_failure():
+    # anything else return 100
+    assert value("what does the fox say ?") == 100
+    assert value("Amin") == 100
