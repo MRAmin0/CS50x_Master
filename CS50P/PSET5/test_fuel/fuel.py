3a69cb5a13a9e31baa63f25c62c858710b275b47
MRAm <125295545+MRAmin0@users.noreply.github.com>
2023-11-23 10:15:51 +0000

    Thu, Nov 23, 2023, 1:45 PM +03:30

--- CS50P/PSET5/test_fuel/fuel.py
+++ CS50P/PSET5/test_fuel/fuel.py
@@ -2,28 +2,11 @@ def main():
     while True:
         # left fuel input
         try:
-        tank = str(input("Fraction: ").strip())
-        # using try except to stop errors in devide
-        try:
-            # spiliting the input
-            u, d = tank.split("/", 1)
-            # cheking if both are numbers
-            if u.isdigit() and d.isdigit():
-                if int(u) <= int(d) and int(d) != 0:
-                    fuel = (int(u) / int(d)) * 100
-                    if fuel >= 99:
-                        print("F")
-                        break
-                    elif fuel < 99 and fuel > 1:
-                        print(f"{fuel:.0f}%")
-                        break
-                    else:
-                        print("E")
-                        break
-        except (ValueError, ZeroDivisionError):
-            pass
-        else:
-            pass
+            fraction = str(input("Fraction: ").strip())
+            print(gauge(convert(fraction)))
+            break
+        except(ValueError,ZeroDivisionError,UnboundLocalError):
+            continue
 
 
 def convert(fraction):
