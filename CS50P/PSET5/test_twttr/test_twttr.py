54e41034b80e292234300730ae1b6110c6164349
MRAm <125295545+MRAmin0@users.noreply.github.com>
2023-11-23 08:35:14 +0000

    Thu, Nov 23, 2023, 12:05 PM +03:30

--- CS50P/PSET5/test_twttr/test_twttr.py
+++ CS50P/PSET5/test_twttr/test_twttr.py
@@ -1,1 +1,10 @@
-from twttr
+from twttr import shorteb
+
+
+def test_shorten():
+    # test if it removes all vowels in a str
+    assert shorten("aeiou") == ""
+    assert shorten("AEIOU") == ""
+    assert shorten("amin") == ""
+    assert shorten("Iran : The permanent land!") == "rn : Th Prmnnt lnd!"
+    assert shorten("1aeu2b333cii4f5ea") == "12b333c4f5"
