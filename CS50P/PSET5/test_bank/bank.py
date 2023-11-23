cecd47d1064ca08c71fa7065efa46564e7c70630
MRAm <125295545+MRAmin0@users.noreply.github.com>
2023-11-23 09:19:17 +0000

    Thu, Nov 23, 2023, 12:49 PM +03:30

---
+++ CS50P/PSET5/test_bank/bank.py
@@ -0,0 +1,12 @@
+def main():
+    # say greetings
+    i = input("Greeting: ").strip().lower()
+     print(f"${value(i)}")
+
+
+def value(greeting):
+# check how much we owe    ...
+
+
+if __name__ == "__main__":
+    main()
