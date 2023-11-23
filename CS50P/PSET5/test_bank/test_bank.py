21217a02bf7c7ad144c6bc16c03e56ae0a7103a5
MRAm <125295545+MRAmin0@users.noreply.github.com>
2023-11-23 09:36:18 +0000

    Thu, Nov 23, 2023, 1:06 PM +03:30

---
+++ CS50P/PSET5/test_bank/test_bank.py
@@ -0,0 +1,14 @@
+from bank import value
+
+
+def test_value_casesenstivity():
+    # if input starts with "hello" return 0
+    assert value("hello") == 0
+    assert value("Hello") == 0
+
+
+
+def test_value_firstletter():
+    # if input starts with "h" but not hello return 20
+    assert value("how u doing?") == 20
+    assert value("hoo ho") == 0
