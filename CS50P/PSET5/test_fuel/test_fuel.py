ea7c3aabcd4d7ee2a412c44fe4b1f9ff2eb5bfa8
MRAm <125295545+MRAmin0@users.noreply.github.com>
2023-11-23 11:05:24 +0000

    Thu, Nov 23, 2023, 2:35 PM +03:30

--- CS50P/PSET5/test_fuel/test_fuel.py
+++ CS50P/PSET5/test_fuel/test_fuel.py
@@ -4,10 +4,10 @@ from fuel import convert, gauge
 
 def test_convert():
     assert convert("0/1") == 0
-    assert convert("1/100") == 1
     assert convert("1/1") == 100
-    assert convert("1/2") == 50
+    # assert convert("1/2") == 50
     assert convert("3/4") == 75
+    assert convert("1/100") == 1
 
     # not int check
     with pytest.raises(ValueError):
@@ -15,7 +15,7 @@ def test_convert():
 
     # Zero devision check
     with pytest.raises(ZeroDivisionError):
-        convert("0/1")
+        convert("1/0")
 
 
 def test_gauge():
@@ -24,9 +24,9 @@ def test_gauge():
     assert gauge(1) == "E"
     assert gauge(3) == "3%"
     assert gauge(50) == "50%"
-    assert gauge(60) == "60%"
-    assert gauge(70) == "70%"
-    assert gauge(80) == "80%"
-    assert gauge(90) == "90%"
+    # assert gauge(60) == "60%"
+    # assert gauge(70) == "70%"
+    assert gauge(74) == "74%"
+    assert gauge(98) == "98%"
     assert gauge(99) == "F"
     assert gauge(100) == "F"
