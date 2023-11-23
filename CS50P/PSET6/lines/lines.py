d7fc7187b8828726287b714fb280687c8fd1bee2
MRAm <125295545+MRAmin0@users.noreply.github.com>
2023-11-23 12:00:27 +0000

    Thu, Nov 23, 2023, 3:30 PM +03:30

--- CS50P/PSET6/lines/lines.py
+++ CS50P/PSET6/lines/lines.py
@@ -1,14 +1,15 @@
 import sys
+
 # we import os to get file Address
 import os
 
 
 def main():
-    #p-file line counter
+    # p-file line counter
     if len(sys.argv) < 2:
         print("Too few command-line arguments")
         sys.exit(1)
-    elif len(sys.argv) >2:
+    elif len(sys.argv) > 2:
         print("Too many command-line arguments")
         sys.exit(1)
     elif not os.path.isfile(sys.argv[1]):
@@ -27,10 +28,19 @@ def counter(python_file):
     rline = 0
 
     with open(python_file, "r") as lines:
-
-        #total line counter
-        tlines = list(enumerate(lines.readlines(), start= 1))
+        # total line counter
+        tlines = list(enumerate(lines.readlines(), start=1))
 
         # check if each line is code or not
         for nline, line in tlines:
-            if 
+            if (
+                lines.strip().startswith("#")
+                or lines.srtip().startswith("\n")
+                or ines.isspace()
+            ):
+                rline += 1
+    return int(tlines[-1][0]) - rline
+
+
+if __name__ == "__main__":
+    main()
