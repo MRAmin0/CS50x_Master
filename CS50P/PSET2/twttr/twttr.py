0503277716f78f62e8c58fdaf45660618bf77273
MRAm <125295545+MRAmin0@users.noreply.github.com>
2023-11-23 08:19:41 +0000

    Thu, Nov 23, 2023, 11:49 AM +03:30

--- CS50P/PSET2/twttr/twttr.py
+++ CS50P/PSET2/twttr/twttr.py
@@ -1,8 +1,18 @@
 vowel = ["a", "o", "u", "i", "e"]
-input = input("Input : ").strip()
 output = ""
 for letter in input:
     if letter.lower() not in vowel:
         output += letter
 
 print(f"Output: {output}")
+def main():
+    input = input("Input : ").strip()
+
+
+
+def shorten(word):
+    ...
+
+
+if __name__ == "__main__":
+    main()
