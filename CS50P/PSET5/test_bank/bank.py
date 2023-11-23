6fd773f2b68545478d3964f3b8762a083bd7f193
MRAm <125295545+MRAmin0@users.noreply.github.com>
2023-11-23 09:23:24 +0000

    Thu, Nov 23, 2023, 12:53 PM +03:30

--- CS50P/PSET5/test_bank/bank.py
+++ CS50P/PSET5/test_bank/bank.py
@@ -6,7 +6,12 @@ def main():
 
 def value(greeting):
 # check how much we owe    ...
-
+if i.startswith("hello"):
+        a = 0
+    elif i.startswith("h"):
+        a = 20
+    else:
+        a = 100
 
 if __name__ == "__main__":
     main()
