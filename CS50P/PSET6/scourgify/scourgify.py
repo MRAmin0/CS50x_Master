8542e1f228a14d1377dd8469b6d647e06dfb2aaf
MRAm <125295545+MRAmin0@users.noreply.github.com>
2023-11-24 18:32:07 +0000

    Fri, Nov 24, 2023, 10:02 PM +03:30

--- CS50P/PSET6/scourgify/scourgify.py
+++ CS50P/PSET6/scourgify/scourgify.py
@@ -1,14 +1,14 @@
 import os
 import sys
-form PIL import Iamge, ImageOps
+from PIL import Iamge, ImageOps
 
 
 def main():
     # check command-line arguments
-    if len(sys.argv) < 2:
+    if len(sys.argv) < 3:
         print("Too few command-line arguments")
         sys.exit(1)
-    elif len(sys.argv) > 2:
+    elif len(sys.argv) > 3:
         print("Too many command-line arguments")
         sys.exit(1)
     elif not os.path.isfile(sys.argv[1]):
