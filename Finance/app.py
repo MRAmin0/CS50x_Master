1395481321d21a0eee605c04c55bd873a7853ffb
MRAm <125295545+MRAmin0@users.noreply.github.com>
2023-11-25 17:27:18 +0000

    Sat, Nov 25, 2023, 8:57 PM +03:30

--- Finance/app.py
+++ Finance/app.py
@@ -186,7 +186,7 @@ def login():
         if len(rows) != 1 or not check_password_hash(
             rows[0]["hash"], request.form.get("password")
         ):
-            return apology("invalid username and/or password", 403)
+            return apology("invalid username and/or password", 400)
 
         # Remember which user has logged in
         session["user_id"] = rows[0]["id"]
@@ -250,9 +250,9 @@ def register():
         if len(username) < 4:
             return apology("Username must be at least 4 characters long!", 403)
 
-        # Ensure username consists only of characters and digits
-        if not username.isalnum():
-            return apology("Username must contain only characters and digits!", 403)
+        # # Ensure username consists only of characters and digits
+        # if not username.isalnum():
+        #     return apology("Username must contain only characters and digits!", 403)
 
         # Ensure password is stronger (has characters, digits, symbols)
         if len(password) < 8:
