8af319d3450168bb519cee84dcb436126f1f250e
MRAm <125295545+MRAmin0@users.noreply.github.com>
2023-11-28 07:08:19 +0000

    Tue, Nov 28, 2023, 10:38 AM +03:30

--- CS50P/PSET7/working/test_working.py
+++ CS50P/PSET7/working/test_working.py
@@ -1,82 +1,63 @@
-# import pytest
-# from working import convert
-
-
-# def test_case():
-#     # check for case insenstivity
-#     assert convert("9:00 AM to 5:00 PM") == "09:00 to 17:00"
-
-
-# def test_max():
-#     # check max time range
-#     assert convert("12:00 PM to 12:00 PM") == "12:00 to 12:00"
-
-
-# def test_mixed():
-#     # Check ValueErrors mixing 12H and 24H time
-#     with pytest.raises(ValueError):
-#         convert("9:00 to 17:00")
-
-
-# def test_to():
-#     # check if ValueError when to is omitted
-#     with pytest.raises(ValueError):
-#         convert(" to ")
-
-
-# def test_outh():
-#     # ValueErrors when hour ranges are exceeded
-#     with pytest.raises(ValueError):
-#         convert("14:00 AM to 14:00 PM")
-
-#     with pytest.raises(ValueError):
-#         convert("0:00 AM to 0:00 PM")
-
-#     with pytest.raises(ValueError):
-#         convert("15 AM to 15 PM")
-
-#     with pytest.raises(ValueError):
-#         convert("0 AM to 0 PM")
-
-
-# def test_outm():
-#     # valueErrors when minute ranges are exceeded
-#     with pytest.raises(ValueError):
-#         convert("12:62 AM to 3:60 PM")
-
-
-# def test_ne():
-#     # valueErrors when nothing is entered
-#     with pytest.raises(ValueError):
-#         convert("")
-
-
-# def test_invformat():
-#     # valueErrors when time values are poorly formatted
-#     with pytest.raises(ValueError):
-#         convert("8AM to 9PM")
-
-#     with pytest.raises(ValueError):
-#         convert("3:21 AM to 7:22 PM")
-
-#     with pytest.raises(ValueError):
-#         convert("10 AM - 6 PM")
 import pytest
 from working import convert
 
 
-def test_convert():
-    assert convert("9 AM to 5 PM") == "09:00 to 17:00"
+def test_case():
+    # check for case insenstivity
     assert convert("9:00 AM to 5:00 PM") == "09:00 to 17:00"
-    assert convert("10 PM to 8 AM") == "22:00 to 08:00"
-    assert convert("10:30 PM to 8:50 AM") == "22:30 to 08:50"
 
-def test_value_error():
+
+def test_max():
+    # check max time range
+    assert convert("12:00 PM to 12:00 PM") == "12:00 to 12:00"
+
+
+def test_mixed():
+    # Check ValueErrors mixing 12H and 24H time
     with pytest.raises(ValueError):
-        convert("9AM - 5PM")
+        convert("9:00 to 17:00")
+
+
+def test_to():
+    # check if ValueError when to is omitted
     with pytest.raises(ValueError):
-        convert("09:00 to 17:00")
+        convert(" to ")
+
+
+def test_outh():
+    # ValueErrors when hour ranges are exceeded
     with pytest.raises(ValueError):
-        convert("15:00 AM to 25:00 PM")
+        convert("14:00 AM to 14:00 PM")
+
     with pytest.raises(ValueError):
-        convert("9:60 AM to 5:60 PM")
+        convert("0:00 AM to 0:00 PM")
+
+    with pytest.raises(ValueError):
+        convert("15 AM to 15 PM")
+
+    with pytest.raises(ValueError):
+        convert("0 AM to 0 PM")
+
+
+def test_outm():
+    # valueErrors when minute ranges are exceeded
+    with pytest.raises(ValueError):
+        convert("12:62 AM to 3:60 PM")
+
+
+def test_ne():
+    # valueErrors when nothing is entered
+    with pytest.raises(ValueError):
+        convert("")
+
+
+def test_invformat():
+    # valueErrors when time values are poorly formatted
+    with pytest.raises(ValueError):
+        convert("8AM to 9PM")
+
+    with pytest.raises(ValueError):
+        convert("3:21 AM to 7:22 PM")
+
+    with pytest.raises(ValueError):
+        convert("10 AM - 6 PM")
--- CS50P/PSET7/working/working.py
+++ CS50P/PSET7/working/working.py
@@ -1,125 +1,87 @@
-# import re
-# import sys
-
-
-# def main():
-#     print(convert(input("Hours: ").strip()))
-
-
-# def convert(s):
-#     # convert 12H to 24H format
-#     times = []
-#     hours = []
-
-#     try:
-#         # find all working hours in sentence
-#         times = re.findall(r"^\d.*\b(?= to)|(?<=to\s).*?[a-z]\b", s, re.IGNORECASE)
-#         # only two time values entered
-#         if len(times) > 2 or len(times) < 2:
-#             raise ValueError
-#         for wt in times:
-#             # check if AM
-#             if " am" in str(wt).lower():
-#                 wt = str(wt).lower().strip(" am")
-
-#                 # check if 12H format complex
-#                 if hm := re.match(r"^(1[0-2]|0?[1-9]):([0-5]?[0-9])$", wt):
-#                     # checck if out of bound
-#                     if int(hm.group(1)) < 12 or int(hm.group(1)) < 1:
-#                         raise ValueError
-#                     elif int(hm.group(1)) == 12:
-#                         hours.append(f"00:{hm.group(2)}")
-
-#                     elif int(hm.group(1)) <= 9:
-#                         hours.append(f"0{hm.group(1)}:{hm.group(2)}")
-
-#                     else:
-#                         hours.append(f"0{hm.group(1)}:{hm.group(2)}")
-#                 # check if 12H simple time
-#                 elif hh := re.match(r"^(1[0-2]|0?[1-9])", wt):
-#                     if int(wt) > 12 or int(wt) < 1:
-#                         raise ValueError
-#                     elif int(hh.group(1)) == 12:
-#                         hours.append(f"00:00")
-
-#                     elif int(hh.group(1)) <= 9:
-#                         hours.append(f"0{hh.group(1)}:00")
-
-#                     else:
-#                         hours.append(f"{hh.group(1)}:00")
-#                 else:
-#                     raise ValueError
-
-#             # check if PM
-#             if " pm" in str(wt).lower():
-#                 wt = str(wt).lower().strip(" am")
-#                 # check if 12H format complex
-#                 if hm := re.match(r"^(1[0-2]|0?[1-9]):([0-5]?[0-9])$", wt):
-#                     # checck if out of bound
-#                     if int(hm.group(1)) > 12 or int(hm.group(1)) < 1:
-#                         raise ValueError
-#                     elif int(hm.group(1)) == 12:
-#                         hours.append(f"12:{hm.group(2)}")
-#                     else:
-#                         hours.append(f"{int(hm.group(1)) + 12}:{hm.group(2)}")
-
-#                 # check if 12 hour simple time
-#                 elif hh := re.match(r"^(1[0-2]|0?[1-9])", wt):
-#                     # check if out of bound
-#                     if int(wt) > 12 or int(wt) < 1:
-#                         raise ValueError
-
-#                     elif int(hh.group(1)) == 12:
-#                         hours.append(f"12:00")
-#                     else:
-#                         hours.append(f"{int(hh.group(1)) + 12}:00")
-#                 else:
-#                     raise ValueError
-#     except ValueError:
-#         raise ValueError
-#     # check if 2 value to return
-#     if len(hours) == 2:
-#         return f"{hours[0]} to {hours[1]}"
-#     else:
-#         raise ValueError
-
-
-# if __name__ == "__main__":
-#     main()
 import re
+import sys
 
 
 def main():
-    print(convert(input("Hours: ")))
+    print(convert(input("Hours: ").strip()))
 
 
 def convert(s):
-    regex = "(0?[1-9]|1[0-2]):?\.?([0-5][0-9])? (AM|PM)"
-    match = re.search(r"^" + regex + " to " + regex + "$", s)
-    if match:
-        from_time = standardize(match.group(1), match.group(2), match.group(3))
-        time = standardize(match.group(4), match.group(5), match.group(6))
-        return f"{from_time} to {time}"
+    # convert 12H to 24H format
+    times = []
+    hours = []
+
+    try:
+        # find all working hours in sentence
+        times = re.findall(r"^\d.*\b(?= to)|(?<=to\s).*?[a-z]\b", s, re.IGNORECASE)
+        # only two time values entered
+        if len(times) > 2 or len(times) < 2:
+            raise ValueError
+        for wt in times:
+            # check if AM
+            if " am" in str(wt).lower():
+                wt = str(wt).lower().strip(" am")
+
+                # check if 12H format complex
+                if hm := re.match(r"^(1[0-2]|0?[1-9]):([0-5]?[0-9])$", wt):
+                    # checck if out of bound
+                    if int(hm.group(1)) < 12 or int(hm.group(1)) < 1:
+                        raise ValueError
+                    elif int(hm.group(1)) == 12:
+                        hours.append(f"00:{hm.group(2)}")
+
+                    elif int(hm.group(1)) <= 9:
+                        hours.append(f"0{hm.group(1)}:{hm.group(2)}")
+
+                    else:
+                        hours.append(f"0{hm.group(1)}:{hm.group(2)}")
+                # check if 12H simple time
+                elif hh := re.match(r"^(1[0-2]|0?[1-9])", wt):
+                    if int(wt) > 12 or int(wt) < 1:
+                        raise ValueError
+                    elif int(hh.group(1)) == 12:
+                        hours.append(f"00:00")
+
+                    elif int(hh.group(1)) <= 9:
+                        hours.append(f"0{hh.group(1)}:00")
+
+                    else:
+                        hours.append(f"{hh.group(1)}:00")
+                else:
+                    raise ValueError
+
+            # check if PM
+            if " pm" in str(wt).lower():
+                wt = str(wt).lower().strip(" am")
+                # check if 12H format complex
+                if hm := re.match(r"^(1[0-2]|0?[1-9]):([0-5]?[0-9])$", wt):
+                    # checck if out of bound
+                    if int(hm.group(1)) > 12 or int(hm.group(1)) < 1:
+                        raise ValueError
+                    elif int(hm.group(1)) == 12:
+                        hours.append(f"12:{hm.group(2)}")
+                    else:
+                        hours.append(f"{int(hm.group(1)) + 12}:{hm.group(2)}")
+
+                # check if 12 hour simple time
+                elif hh := re.match(r"^(1[0-2]|0?[1-9])", wt):
+                    # check if out of bound
+                    if int(wt) > 12 or int(wt) < 1:
+                        raise ValueError
+
+                    elif int(hh.group(1)) == 12:
+                        hours.append(f"12:00")
+                    else:
+                        hours.append(f"{int(hh.group(1)) + 12}:00")
+                else:
+                    raise ValueError
+    except ValueError:
+        raise ValueError
+    # check if 2 value to return
+    if len(hours) == 2:
+        return f"{hours[0]} to {hours[1]}"
     else:
         raise ValueError
-
-
-def standardize(hr, min, x):
-    if hr == "12":
-        if x == "AM":
-            hour = "00"
-        else:
-            hour = "12"
-    else:
-        if x == "AM":
-            hour = f"{int(hr):02}"
-        else:
-            hour = f"{int(hr)+12}"
-    if min == None:
-        minute = "00"
-    else:
-        minute = f"{int(min):02}"
-    return f"{hour}:{minute}"
 
 
 if __name__ == "__main__":
