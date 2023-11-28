8af27eb9648ff61cdb88a84bea5e7d4ce73bb651
MRAm <125295545+MRAmin0@users.noreply.github.com>
2023-11-28 06:18:48 +0000

    Tue, Nov 28, 2023, 9:48 AM +03:30

--- CS50P/PSET7/working/test_working.py
+++ CS50P/PSET7/working/test_working.py
@@ -1,9 +1,11 @@
 import pytest
 from working import convert
 
+
 def test_case():
     # check for case insenstivity
     assert convert("9:00 AM to 5:00 PM") == "09:00 to 17:00"
+
 
 def test_max():
     # check max time range
@@ -19,45 +21,43 @@ def test_mixed():
 def test_to():
     # check if ValueError when to is omitted
     with pytest.raises(ValueError):
-    convert(" to ")
+        convert(" to ")
+
 
 def test_outh():
     # ValueErrors when hour ranges are exceeded
     with pytest.raises(ValueError):
-    convert("14:00 AM to 14:00 PM")
+        convert("14:00 AM to 14:00 PM")
 
     with pytest.raises(ValueError):
-    convert("0:00 AM to 0:00 PM")
+        convert("0:00 AM to 0:00 PM")
 
     with pytest.raises(ValueError):
-    convert("15 AM to 15 PM")
+        convert("15 AM to 15 PM")
 
     with pytest.raises(ValueError):
-    convert("0 AM to 0 PM")
-
-
+        convert("0 AM to 0 PM")
 
 
 def test_outm():
     # valueErrors when minute ranges are exceeded
     with pytest.raises(ValueError):
-    convert("12:62 AM to 3:60 PM")
-
+        convert("12:62 AM to 3:60 PM")
 
 
 def test_ne():
     # valueErrors when nothing is entered
     with pytest.raises(ValueError):
-    convert("")
+        convert("")
+
 
 def test_invformat():
     # valueErrors when time values are poorly formatted
     with pytest.raises(ValueError):
-    convert("8AM to 9PM")
+        convert("8AM to 9PM")
 
     with pytest.raises(ValueError):
-    convert("3:21 AM to 7:22 PM")
+        convert("3:21 AM to 7:22 PM")
 
     with pytest.raises(ValueError):
-    convert("10 AM - 6 PM")
-
+        convert("10 AM - 6 PM")
