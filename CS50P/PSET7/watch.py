9db17ffb87996a0d533d56391b04c18b89819082
MRAm <125295545+MRAmin0@users.noreply.github.com>
2023-11-28 05:21:57 +0000

    Tue, Nov 28, 2023, 8:51 AM +03:30

--- CS50P/PSET7/watch.py
+++ CS50P/PSET7/watch.py
@@ -9,6 +9,7 @@ def main():
 def parse(s):
     # extract youtube link from embed
     try:
+        # positive look behind 
         url = re.search('(?<=embed\/).*?(?=")',s)
         return "https://youtu.be/" + url.group(0)
 
