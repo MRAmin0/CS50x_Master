23c51893e69cb52ab324e3fb2a995df598f62a3e
MRAm <125295545+MRAmin0@users.noreply.github.com>
2023-11-28 04:37:18 +0000

    Tue, Nov 28, 2023, 8:07 AM +03:30

---
+++ CS50P/PSET7/numb3rs/numb3rs.py
@@ -0,0 +1,18 @@
+import re
+import sys
+
+
+def main():
+    # get input from user
+    print(validate(input("IPv4 Address: ").strip()))
+
+
+def validate(ip):
+    ...
+
+
+...
+
+
+if __name__ == "__main__":
+    main()
