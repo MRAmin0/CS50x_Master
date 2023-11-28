12c5198bb58499daa9a0f074b8fe747652b75543
MRAm <125295545+MRAmin0@users.noreply.github.com>
2023-11-28 07:41:47 +0000

    Tue, Nov 28, 2023, 11:11 AM +03:30

--- CS50P/PSET7/um/um.py
+++ CS50P/PSET7/um/um.py
@@ -3,11 +3,13 @@ import sys
 
 
 def main():
+    # get input
     print(count(input("Text: ")))
 
 
 def count(s):
-    ...
+    # return number of ums as an int
+    return len(re.findall(r"^um"))
 
 
 ...
