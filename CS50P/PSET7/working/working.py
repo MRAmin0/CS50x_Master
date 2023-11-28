bc52facaa48e1a54b8525998bad252319dc8600c
MRAm <125295545+MRAmin0@users.noreply.github.com>
2023-11-28 06:02:01 +0000

    Tue, Nov 28, 2023, 9:32 AM +03:30

--- CS50P/PSET7/working/working.py
+++ CS50P/PSET7/working/working.py
@@ -53,21 +53,33 @@ def convert(s):
                     raise ValueError
 
             #check if PM
-            if " pm" in str(wt).lower()
-            wt = str(wt).lower().strip(" pm")
-             if " am" in str(wt).lower():
+            if " pm" in str(wt).lower():
                 wt = str(wt).lower().strip(" am")
                 # check if 12H format complex
                 if hm := re.match(r"^(1[0-2]|0?[1-9]):([0-5]?[0-9])$",wt):
                     # checck if out of bound
-                    if int(hm.group(1)) < 12 or int(hm.group(1)) < 1:
+                    if int(hm.group(1)) > 12 or int(hm.group(1)) < 1:
                         raise ValueError
                     elif int(hm.group(1)) == 12:
-                        hours.append(f"00:{hm.group(2)}")
+                        hours.append(f"12:{hm.group(2)}")
+                    else:
+                        hours.append(f"{int(hm.group(1)) + 12}:{hm.group(2)}")
 
-                    elif int(hm.group(1)) <= 9:
-                        hours.append(f"0{hm.group(1)}:{hm.group(2)}")
 
+                 # check if 12 hour simple time
+                elif hh := re.match(r"^(1[0-2]|0?[1-9])",wt):
+
+                    # check if out of bound
+                    if int(wt) > 12 or int(wt) < 1:
+                        raise ValueError
+
+                    elif int(hh.group(1)) == 12:
+                        hours.append(f"12:00")
+                    else:
+                        hours.append(f"{int(hh.group(1)) + 12}:00")
+                else:
+                    raise ValueError
+ 
 
 
 
