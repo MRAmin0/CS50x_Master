5a1c29386faa40ce992900b33be7beb1172c48f1
MRAm <125295545+MRAmin0@users.noreply.github.com>
2023-11-28 05:31:14 +0000

    Tue, Nov 28, 2023, 9:01 AM +03:30

--- CS50P/PSET7/working/working.py
+++ CS50P/PSET7/working/working.py
@@ -3,12 +3,23 @@ import sys
 
 
 def main():
-    print(convert(input("Hours: ")))
+    print(convert(input("Hours: ").strip()))
 
 
 def convert(s):
-    ...
+    # convert 12H to 24H format
+    times = []
+    hours =[]
 
+    try:
+        # find all working hours in sentence
+        times = re.findall(r"^\d.*\b(?= to)|(?<=to\s).*?[a-z]\b",s,re.IGNORECASE)
+        # only two time values entered
+        if len(times) > 2 or len(times) < 2:
+            raise ValueError
+        for wt in times:
+            # check if AM
+            if " am" in str(wt).lower():
 
 ...
 
