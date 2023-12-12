745fa9f72dc4889fb885281b3201eddcdb9287f9
MRAm <125295545+MRAmin0@users.noreply.github.com>
2023-12-12 07:05:34 +0000

    Tue, Dec 12, 2023, 10:35 AM +03:30

--- CS50P/PSET8/seasons/test_seasons.py
+++ CS50P/PSET8/seasons/test_seasons.py
@@ -1,6 +1,19 @@
 import pytest
-from seasons import textout
+from seasons import date_validate, textout, difcalculate
 
 
-def test_seasons():
-    assert textout("525600") == "Five hundred twenty-five thousand, six hundred"
+ def test_date_validate():
+    with pytest.raises (ValueError):
+      date_validate("June 1, 01")
+    assert date_validate("1999-01-01") == "1999-01-01"
+    assert date_validate("1998-11-15") == "1999-11-15"
+    #assert date_validate("june 1, 01") == "Invalid date"
+    #assert date_validate("2001-01-01") == "2001-01-01"
+def test_difcalculate():
+    assert difcalculate("2020-06-20") == "1774080"
+    assert difcalculate("1998-11-15") == "13132800"
+    assert difcalculate("2001-01-01") == "12012480"
+
+
+ def test_textout():
+    assert textout("1774080") == "One million, seven hundred seventy-four thousan
