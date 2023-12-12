9b9084607121989e5d269e380ba02238df5d4c00
MRAm <125295545+MRAmin0@users.noreply.github.com>
2023-12-12 07:14:17 +0000

    Tue, Dec 12, 2023, 10:44 AM +03:30

--- CS50P/PSET8/seasons/test_seasons.py
+++ CS50P/PSET8/seasons/test_seasons.py
@@ -1,2 +1,7 @@
-form pytest
-import seasons
+import pytest
+from seasons import textout
+
+
+def test_seasons():
+    assert textout("525600") == "Five Hundred twenty-five thousand six hundred minutes"
+    assert textout("748523") == "Seven hundred forty-eight thousand five hundred twenty-three minutes"
