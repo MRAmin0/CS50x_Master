e38cc785a15ac2173e87624635253b4e283a85a8
MRAm <125295545+MRAmin0@users.noreply.github.com>
2023-12-12 06:34:22 +0000

    Tue, Dec 12, 2023, 10:04 AM +03:30

--- CS50P/PSET8/seasons/seasons.py
+++ CS50P/PSET8/seasons/seasons.py
@@ -1,11 +1,20 @@
-from datetime import date
+import sys
+import inflect
+p = inflect.engine()
+from datetime import date, datetime
+
 
 
 def main():
+    a = input("Date of Birth: ")
+    validDate = date_validate(a)
+    difdays = difical(validDate)
+    output = textout(difdays)
+    print(output)
 
 
 
-
-
+def date_validated(birthdate):
+    
 if __name__ == "__main__":
     main()
