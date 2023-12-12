d5c8f816c3190f0f102afcb1304d820e854979da
MRAm <125295545+MRAmin0@users.noreply.github.com>
2023-12-12 06:43:13 +0000

    Tue, Dec 12, 2023, 10:13 AM +03:30

--- CS50P/PSET8/seasons/seasons.py
+++ CS50P/PSET8/seasons/seasons.py
@@ -1,39 +1,40 @@
 import sys
 import inflect
+
 p = inflect.engine()
 from datetime import date, datetime
-
 
 
 def main():
     a = input("Date of Birth: ")
     validDate = date_validate(a)
-    difdays = difical(validDate)
+    difdays = difcal(validDate)
     output = textout(difdays)
     print(output)
 
 
-
-def date_validated(birthdate):
+def date_validate(birthdate):
     try:
         # birthdate ro pas bede agar format sal mah rooz dasht khoorooji bede
         input = date.fromisoformat(birthdate)
-        retrun input
+        return input
     except ValueError:
         sys.exit("Invalid date")
 
 
 # convert int to text
 def textout(text):
-    text = p.number_to_words(text,andword="")
-    retrun text.capitalize( ) + " minutes"
+    text = p.number_to_words(text, andword="")
+    return text.capitalize() + " minutes"
+
 
 # find all difference and convert from days to minutes
 def difcal(days):
     today = date.today()
-    daysDiff= today - days
-    #daysDiff.days * 24 * 64
+    daysDiff = today - days
+    daysDiff.days * 24 * 64
     return daysDiff.days * 24 * 60
+
 
 if __name__ == "__main__":
     main()
