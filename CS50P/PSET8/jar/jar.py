9ef35b60559aaed9004cbc9468fc63ae9bc428b4
MRAm <125295545+MRAmin0@users.noreply.github.com>
2023-12-12 07:56:09 +0000

    Tue, Dec 12, 2023, 11:26 AM +03:30

---
+++ CS50P/PSET8/jar/jar.py
@@ -0,0 +1,43 @@
+
+from jar import Jar
+
+
+def test_init():
+    ...
+
+
+def test_str():
+    jar = Jar()
+    assert str(jar) == ""
+    jar.deposit(1)
+    assert str(jar) == "🍪"
+    jar.deposit(11)
+    assert str(jar) == "🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪"
+
+
+def test_deposit():
+    ...
+
+
+def test_withdraw():
+    ...
+class Jar:
+    def __init__(self, capacity=12):
+        ...
+
+    def __str__(self):
+        ...
+
+    def deposit(self, n):
+        ...
+
+    def withdraw(self, n):
+        ...
+
+    @property
+    def capacity(self):
+        ...
+
+    @property
+    def size(self):
+        ...
