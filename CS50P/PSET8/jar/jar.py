b189dc1b0588f5193fcf175854002e35056c7cd9
MRAm <125295545+MRAmin0@users.noreply.github.com>
2023-12-13 06:49:54 +0000

    Wed, Dec 13, 2023, 10:19 AM +03:30

--- CS50P/PSET8/jar/jar.py
+++ CS50P/PSET8/jar/jar.py
@@ -1,5 +1,5 @@
 class Jar:
-    def __init__(self, capacity=12, size =0):
+    def __init__(self, capacity=12, size=0):
         # initialize a cookie jar with the given capacoty
         self.capacity = capacity
         self.size = size
@@ -7,10 +7,10 @@ class Jar:
     def __str__(self):
         # return a str with 🍪, where is the number of cookies
         return "🍪" * self.size
-    
+
     def deposit(self, n):
         # add n cookies to the cookie jar.
-        if not n > (self.capacity -self.size):
+        if not n > (self.capacity - self.size):
             self.size += n
         else:
             raise ValueError("Too many cookies!")
@@ -30,26 +30,25 @@ class Jar:
 
     @property
     def size(self):
-        #return the number of cookies actually in the cookie jar
+        # return the number of cookies actually in the cookie jar
         return self._size
 
     # setter
     @capacity.setter
-    def capacity(self , capacity):
+    def capacity(self, capacity):
         if not int(capacity) > 0:
             raise ValueError("Not a non-negative integer.")
         self._capacity = capacity
 
     @size.setter
-    def size(self , size):
-        if int(siez) < 0"
+    def size(self, size):
+        if int(size) < 0:
             raise ValueError
         else:
             self._size = size
 
+
 def main():
     # Run the main code
-    if __name__ = "__name__":
+    if __name__ == "__main__":
         main()
-
-
--- CS50P/PSET8/jar/test_jar.py
+++ CS50P/PSET8/jar/test_jar.py
@@ -27,6 +27,7 @@ def test_capacity():
     with pytest.raises(ValueError):
         test1 = Jar("dog")
 
+
 def test_size():
     # new jar = 0
     chocolate_cookie_jar = Jar()
@@ -37,18 +38,19 @@ def test_size():
 
     # check that jar size can not be manually instahtiated with new
     with pytest.raises(ValueError):
-        double_fudge_brownies = Jar(12,-1)
+        double_fudge_brownies = Jar(12, -1)
 
 
 def test_deposit():
-  # check deposit and cookies
-  test1 = Jar(10)
-  test1.deposit(4)
-  assert test1.size == 4
+    # check deposit and cookies
+    test1 = Jar(10)
+    test1.deposit(4)
+    assert test1.size == 4
 
-  check1 = Jar(12, 11)
-  with pytest.raises(ValueError):
-      check1.deposit(2)
+    check1 = Jar(12, 11)
+    with pytest.raises(ValueError):
+        check1.deposit(2)
+
 
 def test_capacity_property():
     # capacity return total capacity if the instance
@@ -59,6 +61,7 @@ def test_capacity_property():
         # return the total size of the instance
         jartest1 = Jar(24)
         assert jartest1.capacity == 24
+
 
 def test_size_property():
     # returns the total size of the instance
@@ -81,13 +84,18 @@ def test_withdraw():
 
 
 def test_str():
+    # __str__ returns right amount of cookie emojis.
     jar = Jar()
     assert str(jar) == ""
+
     jar.deposit(1)
     assert str(jar) == "🍪"
+
+    jar.deposit(3)
+    assert str(jar) == "🍪🍪🍪🍪"
+
     jar.deposit(11)
     assert str(jar) == "🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪🍪"
 
-
-
-
+    jar.withdraw(1)
+    assert str(jar) == "🍪🍪🍪"
