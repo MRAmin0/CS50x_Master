badaa1a15e3eb8291603739bbce5bf1ce5b1d12c
MRAm <125295545+MRAmin0@users.noreply.github.com>
2023-12-13 06:22:23 +0000

    Wed, Dec 13, 2023, 9:52 AM +03:30

--- CS50P/PSET8/jar/test_jar.py
+++ CS50P/PSET8/jar/test_jar.py
@@ -2,9 +2,15 @@ import pytest
 from jar import Jar
 
 def test_init():
-    # class initializes intance objecct (manually and automatically)
+    # class initializes intance objecct (manually and automatically) check
     jartest = Jar(9000,8999)
-    ass
+    assert jartest.capacity == 9000
+    assert jartest.size == 8999
+
+    jar = Jar()
+    assert jar.capacity == 12
+    assert jar.size == 0
+
 
 def test_str():
     jar = Jar()
