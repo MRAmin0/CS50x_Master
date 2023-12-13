a5c94e2bca85b489d87babe8b2afbeb417dfb4bd
MRAm <125295545+MRAmin0@users.noreply.github.com>
2023-12-13 07:50:17 +0000

    Wed, Dec 13, 2023, 11:20 AM +03:30

--- CS50P/PSET8/shirtificate/shirtificate.py
+++ CS50P/PSET8/shirtificate/shirtificate.py
@@ -1,17 +1,25 @@
 from fpdf import FPDF
+
+
 class PDF(FPDF):
     def header(self):
         # image name, X , Y , W , H
-        self.image("shirtificate.png", 10, 65 , 190 , 190)
+        self.image("shirtificate.png", 10, 65, 190, 190)
 
         # setting font and style
-        self.set_font("helvetica","B",45)
+        self.set_font("helvetica", "B", 45)
 
         # allignment
-        self.text(45,45,"CS50 Shirtificate")
+        self.text(45, 45, "CS50 Shirtificate")
 
     def footer(self):
         self.set_font("helvetica", "B", 25)
-        self.set_text_color(255,255,255)
+        self.set_text_color(255, 255, 255)
+        self.text(72, 140, a + " took CS50")
 
 
+# pdf output
+a = input("what's your name ? ").strip().Title()
+filepdf = PDF()
+filepdf.add_page
+filepdf.output("shirtificate.pdf")
