8cbe9e151ff02f25c421aa0201ab1d2406d10231
MRAm <125295545+MRAmin0@users.noreply.github.com>
2023-12-16 17:55:10 +0000

    Sat, Dec 16, 2023, 9:25 PM +03:30

--- CS50P/project/chess.py
+++ CS50P/project/chess.py
@@ -1,24 +1,4 @@
 import chess
-import chess.svg
-from IPython.display import display, SVG
 
-def print_board(board):
-    return SVG(chess.svg.board(board=board))
-
-def main():
-    board = chess.Board()
-    display(print_board(board))
-
-    while not board.is_game_over():
-        move_uci = input("Enter your move (in UCI format, e.g., 'e2e4'): ")
-        if chess.Move.from_uci(move_uci) in board.legal_moves:
-            board.push(chess.Move.from_uci(move_uci))
-        else:
-            print("Invalid move! Try again.")
-
-        display(print_board(board))
-
-    print("Game Over!")
-
-if __name__ == "__main__":
-    main()
+board = chess.Board()
+print(board)
