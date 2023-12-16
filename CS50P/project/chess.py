5910122c4b11344ffd6df660b4284aed731ee798
MRAm <125295545+MRAmin0@users.noreply.github.com>
2023-12-16 17:52:05 +0000

    Sat, Dec 16, 2023, 9:22 PM +03:30

---
+++ CS50P/project/chess.py
@@ -0,0 +1,12 @@
+import chess
+import chess.svg
+
+def print_board(board):
+    print(board)
+
+def main():
+    board = chess.Board()
+    print_board(board)
+
+if __name__ == "__main__":
+    main()
