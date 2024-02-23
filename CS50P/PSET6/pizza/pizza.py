4fd70d5b69ba309f743650d9f1388292cfca92de
MRAm <125295545+MRAmin0@users.noreply.github.com>
2024-02-23 16:45:17 +0000

    Fri, Feb 23, 2024, 8:15 PM +03:30

--- CS50P/PSET6/pizza/pizza.py
+++ CS50P/PSET6/pizza/pizza.py
@@ -13,7 +13,7 @@ def main():
         print("Too many command-line arguments")
         sys.exit(1)
     elif not os.path.isfile(sys.argv[1]):
-        print("File foes not exist")
+        print("File does not exist")
         sys.exit(1)
     elif not sys.argv[1].endswith(".csv"):
         print("Not a CSV file")
---
+++ CS50P/PSET6/pizza/text.py
@@ -0,0 +1,31 @@
+import os
+import sys
+import csv
+from tabulate import tabulate
+
+
+def main():
+    if len(sys.argv) < 2:
+        print("Too few command-line arguments")
+        sys.exit(1)
+    elif len(sys.argv) > 2:
+        print("Too many command-line arguments")
+        sys.exit(1)
+    elif not os.path.isfile(sys.argv[1]):
+        print("File does not exist")
+        sys.exit(1)
+    elif not sys.argv[1].endswith(".csv"):
+        print("Not a CSV file")
+        sys.exit(1)
+    else:
+        tablemaker(sys.argv[1])
+
+
+def tablemaker(user):
+    with open(user, "r") as csv_file:
+        t = csv.DictReader(csv_file, delimiter=",")
+        print(tabulate(t, headers=t.fieldnames, tablefmt="grid"))
+
+
+if __name__ == "__main__":
+    main()
